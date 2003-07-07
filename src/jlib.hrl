91fa7350c657efd3ce5c31efd9c783f43dcae29d
Alexey Shchepin <alexey@process-one.net>
2003-07-07 19:45:22 +0000

    * src/mod_pubsub/mod_pubsub.erl: Pub/sub implementation (not completed yet)
    * src/jlib.hrl: Added pub/sub nemaspaces
    
    SVN Revision: 120

--- src/jlib.hrl
+++ src/jlib.hrl
@@ -5,35 +5,38 @@
 %%% Created :  5 Mar 2003 by Alexey Shchepin <alexey@sevcom.net>
 %%%----------------------------------------------------------------------
 
--define(NS_DISCO_ITEMS, "http://jabber.org/protocol/disco#items").
--define(NS_DISCO_INFO,  "http://jabber.org/protocol/disco#info").
--define(NS_VCARD,       "vcard-temp").
--define(NS_AUTH,        "jabber:iq:auth").
--define(NS_AUTH_ERROR,  "jabber:iq:auth:error").
--define(NS_REGISTER,    "jabber:iq:register").
--define(NS_SEARCH,      "jabber:iq:search").
--define(NS_ROSTER,      "jabber:iq:roster").
--define(NS_PRIVATE,     "jabber:iq:private").
--define(NS_VERSION,     "jabber:iq:version").
--define(NS_TIME,        "jabber:iq:time").
--define(NS_XDATA,       "jabber:x:data").
--define(NS_IQDATA,      "jabber:iq:data").
--define(NS_DELAY,       "jabber:x:delay").
--define(NS_EVENT,       "jabber:x:event").
--define(NS_XCONFERENCE, "jabber:x:conference").
--define(NS_STATS,       "http://jabber.org/protocol/stats").
--define(NS_MUC,         "http://jabber.org/protocol/muc").
--define(NS_MUC_USER,    "http://jabber.org/protocol/muc#user").
--define(NS_MUC_ADMIN,   "http://jabber.org/protocol/muc#admin").
--define(NS_MUC_OWNER,   "http://jabber.org/protocol/muc#owner").
+-define(NS_DISCO_ITEMS,  "http://jabber.org/protocol/disco#items").
+-define(NS_DISCO_INFO,   "http://jabber.org/protocol/disco#info").
+-define(NS_VCARD,        "vcard-temp").
+-define(NS_AUTH,         "jabber:iq:auth").
+-define(NS_AUTH_ERROR,   "jabber:iq:auth:error").
+-define(NS_REGISTER,     "jabber:iq:register").
+-define(NS_SEARCH,       "jabber:iq:search").
+-define(NS_ROSTER,       "jabber:iq:roster").
+-define(NS_PRIVATE,      "jabber:iq:private").
+-define(NS_VERSION,      "jabber:iq:version").
+-define(NS_TIME,         "jabber:iq:time").
+-define(NS_XDATA,        "jabber:x:data").
+-define(NS_IQDATA,       "jabber:iq:data").
+-define(NS_DELAY,        "jabber:x:delay").
+-define(NS_EVENT,        "jabber:x:event").
+-define(NS_XCONFERENCE,  "jabber:x:conference").
+-define(NS_STATS,        "http://jabber.org/protocol/stats").
+-define(NS_MUC,          "http://jabber.org/protocol/muc").
+-define(NS_MUC_USER,     "http://jabber.org/protocol/muc#user").
+-define(NS_MUC_ADMIN,    "http://jabber.org/protocol/muc#admin").
+-define(NS_MUC_OWNER,    "http://jabber.org/protocol/muc#owner").
+-define(NS_PUBSUB,       "http://jabber.org/protocol/pubsub").
+-define(NS_PUBSUB_EVENT, "http://jabber.org/protocol/pubsub#event").
+-define(NS_PUBSUB_OWNER, "http://jabber.org/protocol/pubsub#owner").
 
--define(NS_STREAM,      "http://etherx.jabber.org/streams").
+-define(NS_STREAM,       "http://etherx.jabber.org/streams").
 
--define(NS_STANZAS,     "urn:ietf:params:xml:ns:xmpp-stanzas").
--define(NS_STREAMS,     "urn:ietf:params:xml:ns:xmpp-streams").
+-define(NS_STANZAS,      "urn:ietf:params:xml:ns:xmpp-stanzas").
+-define(NS_STREAMS,      "urn:ietf:params:xml:ns:xmpp-streams").
 
--define(NS_SASL, "urn:ietf:params:xml:ns:xmpp-sasl").
--define(NS_SESSION, "urn:ietf:params:xml:ns:xmpp-session").
+-define(NS_SASL,         "urn:ietf:params:xml:ns:xmpp-sasl").
+-define(NS_SESSION,      "urn:ietf:params:xml:ns:xmpp-session").
 
 % TODO: remove "code" attribute (currently it used for backward-compatibility)
 -define(STANZA_ERROR(Code, Type, Condition),
@@ -44,7 +47,7 @@
 -define(ERR_BAD_REQUEST,
 	?STANZA_ERROR("400", "modify",    "bad-request")).
 -define(ERR_CONFLICT,
-	?STANZA_ERROR("409", "cancel",    "cancel")).
+	?STANZA_ERROR("409", "cancel",    "conflict")).
 -define(ERR_FEATURE_NOT_IMPLEMENTED,
 	?STANZA_ERROR("501", "cancel",    "feature-not-implemented")).
 -define(ERR_FORBIDDEN,
@@ -70,9 +73,9 @@
 -define(ERR_SERVICE_UNAVAILABLE,
 	?STANZA_ERROR("503", "cancel",    "service-unavailable")).
 -define(ERR_SUBSCRIPTION_REQUIRED,
-	?STANZA_ERROR("0", "auth", "subscription-required")).
+	?STANZA_ERROR("0",   "auth",      "subscription-required")).
 -define(ERR_UNEXPECTED_REQUEST,
-	?STANZA_ERROR("0", "wait", "unexpected-request")).
+	?STANZA_ERROR("0",   "wait",      "unexpected-request")).
 %-define(ERR_,
 %	?STANZA_ERROR("", "", "")).
 
