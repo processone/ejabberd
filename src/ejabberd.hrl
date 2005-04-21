abf03f610a31fa00b36e120101e988f5f4b2a523
Mickaël Rémond <mickael.remond@process-one.net>
2005-04-21 21:37:19 +0000

    Changed version ID to version 0.9
    SVN Revision: 325

--- src/ejabberd.hrl
+++ src/ejabberd.hrl
@@ -6,7 +6,7 @@
 %%% Id      : $Id$
 %%%----------------------------------------------------------------------
 
--define(VERSION, "0.8-alpha").
+-define(VERSION, "0.9").
 
 %-define(ejabberd_debug, true).
 %-define(DBGFSM, true).
