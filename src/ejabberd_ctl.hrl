904300ce0402019f066a3578bf60ecf01774406e
Alexey Shchepin <alexey@process-one.net>
2006-02-13 04:05:13 +0000

    * src/ejabberd_ctl.erl: Partially rewritten * src/ejabberd_ctl.hrl: Definitions of exit status codes moved here
    SVN Revision: 506

---
+++ src/ejabberd_ctl.hrl
@@ -0,0 +1,12 @@
+%%%----------------------------------------------------------------------
+%%% File    : ejabberd_ctl.hrl
+%%% Author  : Alexey Shchepin <alexey@sevcom.net>
+%%% Purpose : 
+%%% Created : 13 Feb 2006 by Alexey Shchepin <alexey@sevcom.net>
+%%% Id      : $Id$
+%%%----------------------------------------------------------------------
+
+-define(STATUS_SUCCESS, 0).
+-define(STATUS_ERROR,   1).
+-define(STATUS_USAGE,   2).
+-define(STATUS_BADRPC,  3).
