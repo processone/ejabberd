76522293a5b807858d0fc090dc6ed4e9eb0d7006
Mickaël Rémond <mickael.remond@process-one.net>
2006-12-15 22:18:04 +0000

    * src/mod_echo.erl: The mod_echo can now be stopped without errors. This is not a major issue, but can mislead other developers learning from this example and test module (Thanks to Magnus Henoch) (EJAB-163).
    SVN Revision: 696

--- src/mod_echo.erl
+++ src/mod_echo.erl
@@ -52,7 +52,9 @@ start(Host, Opts) ->
 stop(Host) ->
     Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
     gen_server:call(Proc, stop),
-    supervisor:stop_child(ejabberd_sup, Proc).
+    supervisor:terminate_child(ejabberd_sup, Proc),
+    supervisor:delete_child(ejabberd_sup, Proc).
+
 
 %%====================================================================
 %% gen_server callbacks
