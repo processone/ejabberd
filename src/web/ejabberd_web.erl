56740871f18cbc2309b618ab2febe27813102ee0
Badlop <badlop@process-one.net>
2008-05-31 18:21:43 +0000

    * src/web/ejabberd_web.erl (make_xhtml/2): Allow to include elements in the HTTP header
    SVN Revision: 1344

--- src/web/ejabberd_web.erl
+++ src/web/ejabberd_web.erl
@@ -28,7 +28,7 @@
 -author('alexey@process-one.net').
 
 %% External exports
--export([make_xhtml/1,
+-export([make_xhtml/1, make_xhtml/2,
          error/1]).
 
 -include("ejabberd.hrl").
@@ -42,12 +42,16 @@
 %% that third parties can use ejabberd_web as an "utility" library.
 
 make_xhtml(Els) ->
+    make_xhtml([], Els).
+
+make_xhtml(HeadEls, Els) ->
     {xmlelement, "html", [{"xmlns", "http://www.w3.org/1999/xhtml"},
 			  {"xml:lang", "en"},
 			  {"lang", "en"}],
      [{xmlelement, "head", [],
        [{xmlelement, "meta", [{"http-equiv", "Content-Type"},
-			      {"content", "text/html; charset=utf-8"}], []}]},
+			      {"content", "text/html; charset=utf-8"}], []}
+	| HeadEls]},
       {xmlelement, "body", [], Els}
      ]}.
 
