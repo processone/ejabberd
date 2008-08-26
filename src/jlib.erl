80fcd2eb566628e25da5190cf2cec4a477948961
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-08-26 13:00:35 +0000

    Remove commented-out code and use proper variable names.
    SVN Revision: 1545

--- src/jlib.erl
+++ src/jlib.erl
@@ -763,20 +763,16 @@ to_old_jid(#jid{user = Node, resource = Resource,
     JID#jid{user = Node1, resource = Resource1,
       luser = LNode1, lresource = LResource1}.
 
-short_jid(JID1) ->
-    %JID1 = to_old_jid(JID),
-    {JID1#jid.user, JID1#jid.server, JID1#jid.resource}.
+short_jid(JID) ->
+    {JID#jid.user, JID#jid.server, JID#jid.resource}.
 
 short_bare_jid(JID) ->
-    %JID1 = to_old_jid(exmpp_jid:jid_to_bare_jid(JID)),
-    JID1 = exmpp_jid:jid_to_bare_jid(JID),
-    {JID1#jid.user, JID1#jid.server, JID1#jid.resource}.
+    Bare_JID = exmpp_jid:jid_to_bare_jid(JID),
+    {Bare_JID#jid.user, Bare_JID#jid.server, Bare_JID#jid.resource}.
 
-short_prepd_jid(JID1) ->
-    %JID1 = to_old_jid(JID),
-    {JID1#jid.luser, JID1#jid.lserver, JID1#jid.lresource}.
+short_prepd_jid(JID) ->
+    {JID#jid.luser, JID#jid.lserver, JID#jid.lresource}.
 
 short_prepd_bare_jid(JID) ->
-    %JID1 = to_old_jid(exmpp_jid:jid_to_bare_jid(JID)),
-    JID1 = exmpp_jid:jid_to_bare_jid(JID),
-    {JID1#jid.luser, JID1#jid.lserver, JID1#jid.lresource}.
+    Bare_JID = exmpp_jid:jid_to_bare_jid(JID),
+    {Bare_JID#jid.luser, Bare_JID#jid.lserver, Bare_JID#jid.lresource}.
