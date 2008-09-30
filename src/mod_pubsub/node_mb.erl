43647c6d50b25665c194f86f37154847e21f8bdd
Christophe Romain <christophe.romain@process-one.net>
2008-09-30 08:58:37 +0000

    fix licence header
    SVN Revision: 1587

--- src/mod_pubsub/node_mb.erl
+++ src/mod_pubsub/node_mb.erl
@@ -1,3 +1,28 @@
+%%% ====================================================================
+%%% ``The contents of this file are subject to the Erlang Public License,
+%%% Version 1.1, (the "License"); you may not use this file except in
+%%% compliance with the License. You should have received a copy of the
+%%% Erlang Public License along with this software. If not, it can be
+%%% retrieved via the world wide web at http://www.erlang.org/.
+%%% 
+%%% Software distributed under the License is distributed on an "AS IS"
+%%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%%% the License for the specific language governing rights and limitations
+%%% under the License.
+%%% 
+%%% The Initial Developer of the Original Code is ProcessOne.
+%%% Portions created by ProcessOne are Copyright 2006-2008, ProcessOne
+%%% All Rights Reserved.''
+%%% This software is copyright 2006-2008, ProcessOne.
+%%%
+%%%
+%%% @copyright 2006-2008 ProcessOne
+%%% @author Eric Cestari <eric@ohmforce.com>
+%%% @version {@vsn}, {@date} {@time}
+%%% @end
+%%% ====================================================================
+
+
 %%% @doc The module <strong>{@module}</strong> is the pep microblog PubSub plugin.
 %%% <p> To be used, mod_pubsub must be configured :
 %%% {mod_pubsub,   [ % requires mod_caps
