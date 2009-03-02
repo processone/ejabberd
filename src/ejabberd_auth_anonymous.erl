processone/ejabberd

commit 84cbf96c3772d13e6b6302d8fd4b23d62391c556

o  Document the type of the argument(s) and the returned type of every    functions. o  Add guardians expression to many functions of ejabberd_auth and    ejabberd_auth_anonymous to ensure at an early stage that we were given    the right arguments. Other modules are not changed because they are only    used by ejabberd_auth which already does the check.
PR:		EJABP-1


git-svn-id: https://svn.process-one.net/ejabberd/branches/road-to-exmpp@1863 1613c18e-e6f4-0310-972c-c3fad1d2be73

src/ejabberd_auth_anonymous.erl
@@ -53,12 +53,18 @@
 	 remove_user/3,
 	 plain_password_required/0]).
 
+-include_lib("exmpp/include/exmpp_xmpp.hrl").
+
 -include("ejabberd.hrl").
 -record(anonymous, {us, sid}).
 
-%% Create the anonymous table if at least one virtual host has anonymous features enabled
-%% Register to login / logout events
-start(Host) ->
+%% @spec (Host) -> ok
+%%     Host = string()
+%% @doc Create the anonymous table if at least one virtual host has
+%% anonymous features enabled.
+%% Register to login / logout events.
+
+start(Host) when is_list(Host) ->
     %% TODO: Check cluster mode
     mnesia:create_table(anonymous, [{ram_copies, [node()]},
 				    {type, bag},
@@ -70,13 +76,20 @@ start(Host) ->
 		       ?MODULE, unregister_connection, 100),
     ok.
 
-%% Return true if anonymous is allowed for host or false otherwise
-allow_anonymous(Host) ->
+%% @spec (Host) -> bool()
+%%     Host = string()
+%% @doc Return true if anonymous is allowed for host or false otherwise.
+
+allow_anonymous(Host) when is_list(Host) ->
     lists:member(?MODULE, ejabberd_auth:auth_modules(Host)).
 
-%% Return true if anonymous mode is enabled and if anonymous protocol is SASL
-%% anonymous protocol can be: sasl_anon|login_anon|both
-is_sasl_anonymous_enabled(Host) ->
+%% @spec (Host) -> bool()
+%%     Host = string()
+%% @doc Return true if anonymous mode is enabled and if anonymous
+%% protocol is SASL anonymous.
+%% protocol can be: sasl_anon|login_anon|both
+
+is_sasl_anonymous_enabled(Host) when is_list(Host) ->
     case allow_anonymous(Host) of
 	false -> false;	    
 	true ->
@@ -87,10 +100,13 @@ is_sasl_anonymous_enabled(Host) ->
 	    end
     end.
 
-%% Return true if anonymous login is enabled on the server
+%% @spec (Host) -> bool()
+%%     Host = string()
+%% @doc Return true if anonymous login is enabled on the server.
 %% anonymous login can be use using standard authentication method (i.e. with
 %% clients that do not support anonymous login)
-is_login_anonymous_enabled(Host) ->
+
+is_login_anonymous_enabled(Host) when is_list(Host) ->
     case allow_anonymous(Host) of
 	false -> false;
 	true  ->
@@ -101,9 +117,12 @@ is_login_anonymous_enabled(Host) ->
 	    end
     end.
 
-%% Return the anonymous protocol to use: sasl_anon|login_anon|both
+%% @spec (Host) -> sasl_anon | login_anon | both
+%%     Host = string()
+%% @doc Return the anonymous protocol to use: sasl_anon|login_anon|both.
 %% defaults to login_anon
-anonymous_protocol(Host) ->
+
+anonymous_protocol(Host) when is_list(Host) ->
     case ejabberd_config:get_local_option({anonymous_protocol, Host}) of
 	sasl_anon  -> sasl_anon;
 	login_anon -> login_anon;
@@ -111,16 +130,24 @@ anonymous_protocol(Host) ->
 	_Other     -> sasl_anon
     end.
 
-%% Return true if multiple connections have been allowed in the config file
+%% @spec (Host) -> bool()
+%%     Host = string()
+%% @doc Return true if multiple connections have been allowed in the
+%% config file.
 %% defaults to false
-allow_multiple_connections(Host) ->
+
+allow_multiple_connections(Host) when is_list(Host) ->
     case ejabberd_config:get_local_option({allow_multiple_connections, Host}) of
 	true -> true;
 	_Other -> false
     end.
 
-%% Check if user exist in the anonymus database
-anonymous_user_exist(User, Server) ->
+%% @spec (User, Server) -> bool()
+%%     User = string()
+%%     Server = string()
+%% @doc Check if user exist in the anonymus database.
+
+anonymous_user_exist(User, Server) when is_list(User), is_list(Server) ->
     LUser = exmpp_stringprep:nodeprep(User),
     LServer = exmpp_stringprep:nameprep(Server),
     US = {LUser, LServer},
@@ -131,16 +158,26 @@ anonymous_user_exist(User, Server) ->
 	    true
     end.
 
-%% Remove connection from Mnesia tables
-remove_connection(SID, LUser, LServer) ->
+%% @spec (SID, LUser, LServer) -> term()
+%%     SID = term()
+%%     LUser = string()
+%%     LServer = string()
+%% @doc Remove connection from Mnesia tables.
+
+remove_connection(SID, LUser, LServer) when is_list(LUser), is_list(LServer) ->
     US = {LUser, LServer},
     F = fun() ->
 		mnesia:delete_object({anonymous, US, SID})
         end,
     mnesia:transaction(F).
 
-%% Register connection
-register_connection(SID, JID, Info) ->
+%% @spec (SID, JID, Info) -> term()
+%%     SID = term()
+%%     JID = exmpp_jid:jid()
+%%     Info = [term()]
+%% @doc Register connection.
+
+register_connection(SID, JID, Info) when ?IS_JID(JID) ->
     LUser = exmpp_jid:lnode(JID),
     LServer = exmpp_jid:ldomain(JID),
     case proplists:get_value(auth_module, Info) of
@@ -155,26 +192,40 @@ register_connection(SID, JID, Info) ->
             ok
     end.
 
-%% Remove an anonymous user from the anonymous users table
-unregister_connection(SID, JID, _) ->
+%% @spec (SID, JID, Ignored) -> term()
+%%     SID = term()
+%%     JID = exmpp_jid:jid()
+%%     Ignored = term()
+%% @doc Remove an anonymous user from the anonymous users table.
+
+unregister_connection(SID, JID, _) when ?IS_JID(JID) ->
     LUser = exmpp_jid:lnode(JID),
     LServer = exmpp_jid:ldomain(JID),
     purge_hook(anonymous_user_exist(LUser, LServer),
 	       LUser, LServer),
     remove_connection(SID, LUser, LServer).
 
-%% Launch the hook to purge user data only for anonymous users
+%% @spec (bool(), LUser, LServer) -> term()
+%%     LUser = string()
+%%     LServer = string()
+%% @doc Launch the hook to purge user data only for anonymous users.
+
 purge_hook(false, _LUser, _LServer) ->
     ok;
-purge_hook(true, LUser, LServer) ->
+purge_hook(true, LUser, LServer) when is_list(LUser), is_list(LServer) ->
     ejabberd_hooks:run(anonymous_purge_hook, LServer, [LUser, LServer]).
 
 %% ---------------------------------
 %% Specific anonymous auth functions
 %% ---------------------------------
 
-%% When anonymous login is enabled, check the password for permenant users
-%% before allowing access
+%% @spec (User, Server, Password) -> bool()
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%% @doc When anonymous login is enabled, check the password for
+%% permenant users before allowing access.
+
 check_password(User, Server, Password) ->
     check_password(User, Server, Password, undefined, undefined).
 check_password(User, Server, _Password, _StreamID, _Digest) ->
@@ -185,6 +236,10 @@ check_password(User, Server, _Password, _StreamID, _Digest) ->
 	true  -> false;
 	false -> login(User, Server)
     end.
+
+%% @spec (User, Server) -> bool()
+%%     User = string()
+%%     Server = string()
 
 login(User, Server) ->
     case is_login_anonymous_enabled(Server) of
@@ -200,8 +255,13 @@ login(User, Server) ->
 	    end
     end.
 
-%% When anonymous login is enabled, check that the user is permanent before
-%% changing its password
+%% @spec (User, Server, Password) -> ok | {error, not_allowed}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%% @doc When anonymous login is enabled, check that the user is
+%% permanent before changing its password.
+
 set_password(User, Server, _Password) ->
     case anonymous_user_exist(User, Server) of
 	true ->
@@ -210,21 +270,40 @@ set_password(User, Server, _Password) ->
 	    {error, not_allowed}
     end.
 
-%% When anonymous login is enabled, check if permanent users are allowed on
-%% the server:
+%% @spec (User, Server, Password) -> {error, not_allowed}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%% @doc When anonymous login is enabled, check if permanent users are
+%% allowed on the server:
+
 try_register(_User, _Server, _Password) ->
     {error, not_allowed}.
+
+%% @spec () -> nil()
 
 dirty_get_registered_users() ->
     [].
 
+%% @spec (Server) -> nil()
+%%     Server = string()
+
 get_vh_registered_users(_Server) ->
     [].
 
+%% @spec (User, Server) -> Password | false
+%%     User = string()
+%%     Server = string()
+%%     Password = nil()
+%% @doc Return password of permanent user or false for anonymous users.
 
-%% Return password of permanent user or false for anonymous users
 get_password(User, Server) ->
     get_password(User, Server, "").
+
+%% @spec (User, Server, DefaultValue) -> DefaultValue | false
+%%     User = string()
+%%     Server = string()
+%%     DefaultValue = string()
 
 get_password(User, Server, DefaultValue) ->
     case anonymous_user_exist(User, Server) of
@@ -236,16 +315,31 @@ get_password(User, Server, DefaultValue) ->
 	    false
     end.
 
-%% Returns true if the user exists in the DB or if an anonymous user is logged
-%% under the given name
+%% @spec (User, Server) -> bool()
+%%     User = string()
+%%     Server = string()
+%% @doc Returns true if the user exists in the DB or if an anonymous
+%% user is logged under the given name.
+
 is_user_exists(User, Server) ->
     anonymous_user_exist(User, Server).
+
+%% @spec (User, Server) -> {error, not_allowed}
+%%     User = string()
+%%     Server = string()
 
 remove_user(_User, _Server) ->
     {error, not_allowed}.
 
+%% @spec (User, Server, Password) -> {error, not_allowed}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+
 remove_user(_User, _Server, _Password) ->
     not_allowed.
+
+%% @spec () -> bool()
 
 plain_password_required() ->
     false.

src/ejabberd_auth.erl
@@ -56,9 +56,16 @@
 
 -include("ejabberd.hrl").
 
+%% @type authmodule() = ejabberd_auth_anonymous | ejabberd_auth_external |
+%%                      ejabberd_auth_internal | ejabberd_auth_ldap |
+%%                      ejabberd_auth_odbc | ejabberd_auth_pam | atom().
+
 %%%----------------------------------------------------------------------
 %%% API
 %%%----------------------------------------------------------------------
+
+%% @spec () -> term()
+
 start() ->
     lists:foreach(
       fun(Host) ->
@@ -68,42 +75,56 @@ start() ->
 		end, auth_modules(Host))
       end, ?MYHOSTS).
 
-plain_password_required(Server) ->
+%% @spec (Server) -> bool()
+%%     Server = string()
+
+plain_password_required(Server) when is_list(Server) ->
     lists:any(
       fun(M) ->
 	      M:plain_password_required()
       end, auth_modules(Server)).
 
+%% @spec (User, Server, Password) -> bool()
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
 %% @doc Check if the user and password can login in server.
-%% @spec (User::string(), Server::string(), Password::string()) ->
-%%     true | false
-check_password(User, Server, Password) ->
+
+check_password(User, Server, Password)
+  when is_list(User), is_list(Server), is_list(Password) ->
     lists:any(
       fun(M) ->
 	      M:check_password(User, Server, Password)
       end, auth_modules(Server)).
 
+%% @spec (User, Server, Password, StreamID, Digest) -> bool()
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%%     StreamID = string()
+%%     Digest = string()
 %% @doc Check if the user and password can login in server.
-%% @spec (User::string(), Server::string(), Password::string(),
-%%        StreamID::string(), Digest::string()) ->
-%%     true | false
-check_password(User, Server, Password, StreamID, Digest) ->
+
+check_password(User, Server, Password, StreamID, Digest)
+  when is_list(User), is_list(Server), is_list(Password),
+  is_list(StreamID), is_list(Digest) ->
     lists:any(
       fun(M) ->
 	      M:check_password(User, Server, Password, StreamID, Digest)
       end, auth_modules(Server)).
 
+%% @spec (User, Server, Password) -> {true, AuthModule} | false
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%%     AuthModule = authmodule()
 %% @doc Check if the user and password can login in server.
 %% The user can login if at least an authentication method accepts the user
 %% and the password.
 %% The first authentication method that accepts the credentials is returned.
-%% @spec (User::string(), Server::string(), Password::string()) ->
-%%     {true, AuthModule} | false
-%% where
-%%   AuthModule = ejabberd_auth_anonymous | ejabberd_auth_external
-%%                 | ejabberd_auth_internal | ejabberd_auth_ldap
-%%                 | ejabberd_auth_odbc | ejabberd_auth_pam
-check_password_with_authmodule(User, Server, Password) ->
+
+check_password_with_authmodule(User, Server, Password)
+  when is_list(User), is_list(Server), is_list(Password) ->
     Res = lists:dropwhile(
 	    fun(M) ->
 		    not apply(M, check_password,
@@ -114,7 +135,17 @@ check_password_with_authmodule(User, Server, Password) ->
 	[AuthMod | _] -> {true, AuthMod}
     end.
 
-check_password_with_authmodule(User, Server, Password, StreamID, Digest) ->
+%% @spec (User, Server, Password, StreamID, Digest) -> {true, AuthModule} | false
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%%     StreamID = string()
+%%     Digest = string()
+%%     AuthModule = authmodule()
+
+check_password_with_authmodule(User, Server, Password, StreamID, Digest)
+  when is_list(User), is_list(Server), is_list(Password),
+  is_list(StreamID), is_list(Digest) ->
     Res = lists:dropwhile(
 	    fun(M) ->
 		    not apply(M, check_password,
@@ -125,13 +156,17 @@ check_password_with_authmodule(User, Server, Password, StreamID, Digest) ->
 	[AuthMod | _] -> {true, AuthMod}
     end.
 
-%% @spec (User::string(), Server::string(), Password::string()) ->
-%%       ok | {error, ErrorType}
-%% where ErrorType = empty_password | not_allowed | invalid_jid
+%% @spec (User, Server, Password) -> ok | {error, ErrorType}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%%     ErrorType = empty_password | not_allowed | invalid_jid
+
 set_password(_User, _Server, "") ->
     %% We do not allow empty password
     {error, empty_password};
-set_password(User, Server, Password) ->
+set_password(User, Server, Password)
+  when is_list(User), is_list(Server), is_list(Password) ->
     lists:foldl(
       fun(M, {error, _}) ->
 	      M:set_password(User, Server, Password);
@@ -140,10 +175,15 @@ set_password(User, Server, Password) ->
       end, {error, not_allowed}, auth_modules(Server)).
 
 %% @spec (User, Server, Password) -> {atomic, ok} | {atomic, exists} | {error, not_allowed}
+%%     User = string()
+%%     Server = string()
+%%     Password = string() | nil()
+
 try_register(_User, _Server, "") ->
     %% We do not allow empty password
     {error, not_allowed};    
-try_register(User, Server, Password) ->
+try_register(User, Server, Password)
+  when is_list(User), is_list(Server), is_list(Password) ->
     case is_user_exists(User,Server) of
 	true ->
 	    {atomic, exists};
@@ -168,27 +208,48 @@ try_register(User, Server, Password) ->
 	    end
     end.
 
-%% Registered users list do not include anonymous users logged
+%% @spec () -> [{LUser, LServer}]
+%%     LUser = string()
+%%     LServer = string()
+%% @doc Registered users list do not include anonymous users logged.
+
 dirty_get_registered_users() ->
     lists:flatmap(
       fun(M) ->
 	      M:dirty_get_registered_users()
       end, auth_modules()).
 
-%% Registered users list do not include anonymous users logged
-get_vh_registered_users(Server) ->
+%% @spec (Server) -> [{LUser, LServer}]
+%%     Server = string()
+%%     LUser = string()
+%%     LServer = string()
+%% @doc Registered users list do not include anonymous users logged.
+
+get_vh_registered_users(Server) when is_list(Server) ->
     lists:flatmap(
       fun(M) ->
 	      M:get_vh_registered_users(Server)
       end, auth_modules(Server)).
 
-get_vh_registered_users(Server, Opts) ->
+%% @spec (Server, Opts) -> [{LUser, LServer}]
+%%     Server = string()
+%%     Opts = [{Opt, Val}]
+%%         Opt = atom()
+%%         Val = term()
+%%     LUser = string()
+%%     LServer = string()
+
+get_vh_registered_users(Server, Opts) when is_list(Server) ->
     lists:flatmap(
       fun(M) ->
 	      M:get_vh_registered_users(Server, Opts)
       end, auth_modules(Server)).
 
-get_vh_registered_users_number(Server) ->
+%% @spec (Server) -> Users_Number
+%%     Server = string()
+%%     Users_Number = integer()
+
+get_vh_registered_users_number(Server) when is_list(Server) ->
     lists:sum(
       lists:map(
 	fun(M) ->
@@ -201,7 +262,14 @@ get_vh_registered_users_number(Server) ->
 		end
 	end, auth_modules(Server))).
 
-get_vh_registered_users_number(Server, Opts) ->
+%% @spec (Server, Opts) -> Users_Number
+%%     Server = string()
+%%     Opts = [{Opt, Val}]
+%%         Opt = atom()
+%%         Val = term()
+%%     Users_Number = integer()
+
+get_vh_registered_users_number(Server, Opts) when is_list(Server) ->
     lists:sum(
       lists:map(
 	fun(M) ->
@@ -214,9 +282,13 @@ get_vh_registered_users_number(Server, Opts) ->
 		end
 	end, auth_modules(Server))).
 
+%% @spec (User, Server) -> Password | false
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
 %% @doc Get the password of the user.
-%% @spec (User::string(), Server::string()) -> Password::string()
-get_password(User, Server) ->
+
+get_password(User, Server) when is_list(User), is_list(Server) ->
     lists:foldl(
       fun(M, false) ->
 	      M:get_password(User, Server);
@@ -224,7 +296,13 @@ get_password(User, Server) ->
 	      Password
       end, false, auth_modules(Server)).
 
-get_password_s(User, Server) ->
+%% @spec (User, Server) -> Password | nil()
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%% @doc Get the password of the user.
+
+get_password_s(User, Server) when is_list(User), is_list(Server) ->
     case get_password(User, Server) of
 	false ->
 	    "";
@@ -232,10 +310,15 @@ get_password_s(User, Server) ->
 	    Password
     end.
 
+%% @spec (User, Server) -> {Password, AuthModule} | {false, none}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%%     AuthModule = authmodule()
 %% @doc Get the password of the user and the auth module.
-%% @spec (User::string(), Server::string()) ->
-%%     {Password::string(), AuthModule::atom()} | {false, none}
-get_password_with_authmodule(User, Server) ->
+
+get_password_with_authmodule(User, Server)
+  when is_list(User), is_list(Server) ->
     lists:foldl(
       fun(M, {false, _}) ->
 	      {M:get_password(User, Server), M};
@@ -243,26 +326,39 @@ get_password_with_authmodule(User, Server) ->
 	      {Password, AuthModule}
       end, {false, none}, auth_modules(Server)).
 
-%% Returns true if the user exists in the DB or if an anonymous user is logged
-%% under the given name
-is_user_exists(User, Server) ->
+%% @spec (User, Server) -> bool()
+%%     User = string()
+%%     Server = string()
+%% @doc Returns true if the user exists in the DB or if an anonymous
+%% user is logged under the given name.
+
+is_user_exists(User, Server) when is_list(User), is_list(Server) ->
     lists:any(
       fun(M) ->
 	      M:is_user_exists(User, Server)
       end, auth_modules(Server)).
 
-%% Check if the user exists in all authentications module except the module
-%% passed as parameter
-is_user_exists_in_other_modules(Module, User, Server) ->
+%% @spec (Module, User, Server) -> bool
+%%     Module = authmodule()
+%%     User = string()
+%%     Server = string()
+%% @doc Check if the user exists in all authentications module except
+%% the module passed as parameter.
+
+is_user_exists_in_other_modules(Module, User, Server)
+  when is_list(User), is_list(Server) ->
     lists:any(
       fun(M) ->
 	      M:is_user_exists(User, Server)
       end, auth_modules(Server)--[Module]).
 
 %% @spec (User, Server) -> ok | error | {error, not_allowed}
+%%     User = string()
+%%     Server = string()
 %% @doc Remove user.
 %% Note: it may return ok even if there was some problem removing the user.
-remove_user(User, Server) ->
+
+remove_user(User, Server) when is_list(User), is_list(Server) ->
     R = lists:foreach(
       fun(M) ->
 	      M:remove_user(User, Server)
@@ -274,11 +370,16 @@ remove_user(User, Server) ->
     R.
 
 %% @spec (User, Server, Password) -> ok | not_exists | not_allowed | bad_request | error
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
 %% @doc Try to remove user if the provided password is correct.
 %% The removal is attempted in each auth method provided:
 %% when one returns 'ok' the loop stops;
 %% if no method returns 'ok' then it returns the error message indicated by the last method attempted.
-remove_user(User, Server, Password) ->
+
+remove_user(User, Server, Password)
+  when is_list(User), is_list(Server), is_list(Password) ->
     R = lists:foldl(
       fun(_M, ok = Res) ->
 	      Res;
@@ -295,8 +396,11 @@ remove_user(User, Server, Password) ->
 %%%----------------------------------------------------------------------
 %%% Internal functions
 %%%----------------------------------------------------------------------
-%% Return the lists of all the auth modules actually used in the
-%% configuration
+
+%% @spec () -> [authmodule()]
+%% @doc Return the lists of all the auth modules actually used in the
+%% configuration.
+
 auth_modules() ->
     lists:usort(
       lists:flatmap(
@@ -304,8 +408,11 @@ auth_modules() ->
 		auth_modules(Server)
 	end, ?MYHOSTS)).
 
-%% Return the list of authenticated modules for a given host
-auth_modules(Server) ->
+%% @spec (Server) -> [authmodule()]
+%%     Server = string()
+%% @doc Return the list of authenticated modules for a given host.
+
+auth_modules(Server) when is_list(Server) ->
     LServer = exmpp_stringprep:nameprep(Server),
     Method = ejabberd_config:get_local_option({auth_method, LServer}),
     Methods = if

src/ejabberd_auth_external.erl
@@ -46,19 +46,42 @@
 %%%----------------------------------------------------------------------
 %%% API
 %%%----------------------------------------------------------------------
+
+%% @spec (Host) -> ok
+%%     Host = string()
+
 start(Host) ->
     extauth:start(
       Host, ejabberd_config:get_local_option({extauth_program, Host})),
     ok.
 
+%% @spec () -> bool()
+
 plain_password_required() ->
     true.
+
+%% @spec (User, Server, Password) -> bool()
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
 
 check_password(User, Server, Password) ->
     extauth:check_password(User, Server, Password) andalso Password /= "".
 
+%% @spec (User, Server, Password, StreamID, Digest) -> bool()
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+%%     StreamID = string()
+%%     Digest = string()
+
 check_password(User, Server, Password, _StreamID, _Digest) ->
     check_password(User, Server, Password).
+
+%% @spec (User, Server, Password) -> ok | {error, unknown_problem}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
 
 set_password(User, Server, Password) ->
     case extauth:set_password(User, Server, Password) of
@@ -66,28 +89,59 @@ set_password(User, Server, Password) ->
 	_ -> {error, unknown_problem}
     end.
 
+%% @spec (User, Server, Password) -> {error, not_allowed}
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
+
 try_register(_User, _Server, _Password) ->
     {error, not_allowed}.
 
-%% TODO
-%% Return the list of all users handled by external
+%% @spec () -> nil()
+%% @todo Write it.
+%% @doc Return the list of all users handled by external.
+
 dirty_get_registered_users() ->
     [].
+
+%% @spec (Server) -> nil()
+%%     Server = string()
 
 get_vh_registered_users(_Server) ->
     [].
 
+%% @spec (User, Server) -> bool()
+%%     User = string()
+%%     Server = string()
+
 get_password(_User, _Server) ->
     false.
+
+%% @spec (User, Server) -> nil()
+%%     User = string()
+%%     Server = string()
 
 get_password_s(_User, _Server) ->
     "".
 
+%% @spec (User, Server) -> bool()
+%%     User = string()
+%%     Server = string()
+
 is_user_exists(User, Server) ->
     extauth:is_user_exists(User, Server).
 
+%% @spec (User, Server) -> {error, not_allowed}
+%%     User = string()
+%%     Server = string()
+
 remove_user(_User, _Server) ->
     {error, not_allowed}.
+
+%% @spec (User, Server, Password) -> not_allowed
+%%     User = string()
+%%     Server = string()
+%%     Password = string()
 
 remove_user(_User, _Server, _Password) ->
     not_allowed.

commit 2c35514537066b3970b449f0fa266a1232ffd508

Correct include declaration (thanks to badlop). Bug was introduced in r1863
SVN Revision: 1935

src/ejabberd_auth_anonymous.erl
@@ -53,7 +53,7 @@
 	 remove_user/3,
 	 plain_password_required/0]).
 
--include_lib("exmpp/include/exmpp_xmpp.hrl").
+-include_lib("exmpp/include/exmpp.hrl").
 
 -include("ejabberd.hrl").
 -record(anonymous, {us, sid}).