fe297e1deaba19809286ebb9b3ad724fffef9047
Badlop <badlop@process-one.net>
2009-03-03 19:03:56 +0000

    Merge 1879 from trunk.
    * src/mod_muc/mod_muc_log.erl: Prevent XSS in MUC logs by
    linkifying only a few known protocols (EJAB-850)
    
    SVN Revision: 1945

--- src/mod_muc/mod_muc_log.erl
+++ src/mod_muc/mod_muc_log.erl
@@ -767,7 +767,8 @@ htmlize2(S1, NoFollow) ->
     S2 = element(2, regexp:gsub(S1, "\\&", "\\&amp;")),
     S3 = element(2, regexp:gsub(S2, "<", "\\&lt;")),
     S4 = element(2, regexp:gsub(S3, ">", "\\&gt;")),
-    S5 = element(2, regexp:gsub(S4, "[-+.a-zA-Z0-9]+://[^] )\'\"}]+", link_regexp(NoFollow))),
+    S5 = element(2, regexp:gsub(S4, "(http|https|ftp|mailto|xmpp)://[^] )\'\"}]+",
+				link_regexp(NoFollow))),
     %% Remove 'right-to-left override' unicode character 0x202e
     element(2, regexp:gsub(S5, [226,128,174], "[RLO]")).
 
