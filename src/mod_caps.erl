0770e0eee4deb9541e944a3712694f5f5a9de1e2
Christophe Romain <christophe.romain@process-one.net>
2009-04-14 12:46:55 +0000

    after check, last commit was useless
    SVN Revision: 2019

--- src/mod_caps.erl
+++ src/mod_caps.erl
@@ -179,8 +179,8 @@ receive_packet(From, To, {xmlelement, "presence", Attrs, Els}) ->
 	ok;
     "subscribe" ->
 	ok;
-%    "subscribed" ->  % this my be useless, to be checked
-%	ok;
+    "subscribed" ->
+	ok;
     "unsubscribe" ->
 	ok;
     "unsubscribed" ->
