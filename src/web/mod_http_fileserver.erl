e43271216fa5137648c2e955062c1d1f1cea3292
Badlop <badlop@process-one.net>
2009-06-16 13:45:57 +0000

    Add forgotten copyright and license notices.
    SVN Revision: 2181

--- src/web/mod_http_fileserver.erl
+++ src/web/mod_http_fileserver.erl
@@ -2,8 +2,28 @@
 %%% File    : mod_http_fileserver.erl
 %%% Author  : Massimiliano Mirra <mmirra [at] process-one [dot] net>
 %%% Purpose : Simple file server plugin for embedded ejabberd web server
-%%% Created : 26 Sep 2008 by Badlop <badlop@process-one.net>
-%%%-------------------------------------------------------------------
+%%% Created :
+%%%
+%%%
+%%% ejabberd, Copyright (C) 2002-2009   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
+%%%----------------------------------------------------------------------
+
 -module(mod_http_fileserver).
 -author('mmirra@process-one.net').
 
