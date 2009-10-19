0095f4fbf9c97dad563d999e5fa13afcb478f893
Badlop <badlop@process-one.net>
2009-10-19 14:53:39 +0000

    Provide send_timeout_close option in TCP only in R13B or higher (EJAB-926)
    SVN Revision: 2682

--- src/ejabberd_s2s_out.erl
+++ src/ejabberd_s2s_out.erl
@@ -257,11 +257,15 @@ open_socket1(Host, Port) ->
 open_socket2(Type, Addr, Port) ->
     ?DEBUG("s2s_out: connecting to ~p:~p~n", [Addr, Port]),
     Timeout = outgoing_s2s_timeout(),
+    SockOpts = case erlang:system_info(otp_release) >= "R13B" of
+	true -> [{send_timeout_close, true}];
+	false -> []
+    end,
     case (catch ejabberd_socket:connect(Addr, Port,
 					[binary, {packet, 0},
 					 {send_timeout, ?TCP_SEND_TIMEOUT},
                                          {send_timeout_close, true},
-					 {active, false}, Type],
+					 {active, false}, Type | SockOpts],
 					Timeout)) of
 	{ok, _Socket} = R -> R;
 	{error, Reason} = R ->
