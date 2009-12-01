6ce660b9c2ace37798140576cec4ff18f072f0f4
Badlop <badlop@process-one.net>
2009-12-01 20:00:33 +0000

    IQ sent to resource not connected, return service-unavailable instead of recipient-unavailable (EJAB-1045)
    SVN Revision: 2768

--- src/ejabberd_sm.erl
+++ src/ejabberd_sm.erl
@@ -481,7 +481,7 @@ do_route(From, To, Packet) ->
 				_ ->
 				    Err =
 					jlib:make_error_reply(
-					  Packet, ?ERR_RECIPIENT_UNAVAILABLE),
+					  Packet, ?ERR_SERVICE_UNAVAILABLE),
 				    ejabberd_router:route(To, From, Err)
 			    end;
 			_ ->
