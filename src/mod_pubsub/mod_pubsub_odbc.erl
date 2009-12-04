40e06b55c0c6a9401753b7a799491905fa253321
Badlop <badlop@process-one.net>
2009-12-04 18:36:07 +0000

    Typo in mod_pubsub_odbc breaks Service Discovery and more (EJAB-1115)
    SVN Revision: 2785

--- src/mod_pubsub/mod_pubsub_odbc.erl
+++ src/mod_pubsub/mod_pubsub_odbc.erl
@@ -445,7 +445,7 @@ disco_local_features(Acc, _From, To, <<>>, _Lang) ->
     end,
     {result, Feats ++ lists:map(fun(Feature) ->
 	?NS_PUBSUB_s++"#"++Feature
-    end, features(Host, []))};
+    end, features(Host, <<>>))};
 disco_local_features(Acc, _From, _To, _Node, _Lang) ->
     Acc.
 
