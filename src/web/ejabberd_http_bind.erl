fc3221422fc978a7f9515b34ed3a4b2cb113610e
Christopher tofu Zorn <tofu@stanziq.com>
2010-05-05 15:57:11 -0400

    Revert "try async_dirty on bosh"
    This reverts commit d73a6ab68fb9a7978901a32f274bee036f3c2ad0.

--- src/web/ejabberd_http_bind.erl
+++ src/web/ejabberd_http_bind.erl
@@ -264,7 +264,7 @@ handle_session_start(Pid, XmppDomain, Sid, Rid, Attrs,
 	end,
     XmppVersion = xml:get_attr_s("xmpp:version", Attrs),
     ?DEBUG("Create session: ~p", [Sid]),
-    mnesia:async_dirty(
+    mnesia:transaction(
       fun() ->
 	      mnesia:write(
 		#http_bind{id = Sid,
@@ -528,7 +528,7 @@ handle_info(_, StateName, StateData) ->
 %%----------------------------------------------------------------------
 terminate(_Reason, _StateName, StateData) ->
     ?DEBUG("terminate: Deleting session ~s", [StateData#state.id]),
-    mnesia:async_dirty(
+    mnesia:transaction(
       fun() ->
 	      mnesia:delete({http_bind, StateData#state.id})
       end),
