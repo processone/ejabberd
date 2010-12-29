ac873281d7dd6ac21421574a7e176f2f7a842462
W. Andrew Loe III <andrew@andrewloe.com>
2010-12-28 16:47:33 -0800

    Remove a crasher, the room is a binary not a string().

--- src/mod_muc/mod_muc_room.erl
+++ src/mod_muc/mod_muc_room.erl
@@ -784,7 +784,7 @@ terminate(Reason, _StateName, StateData) ->
 	       tab_remove_online_user(LJID, StateData)
        end, [], StateData#state.users),
     add_to_log(room_existence, stopped, StateData),
-    mod_muc:room_destroyed(StateData#state.host, list_to_binary(StateData#state.room), self(),
+    mod_muc:room_destroyed(StateData#state.host, StateData#state.room, self(),
 			   StateData#state.server_host),
     ok.
 
