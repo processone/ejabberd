16a97ab9038e98b14b03227656a1329be564ec26
Badlop <badlop@process-one.net>
2011-01-11 16:22:22 +0100

    Handle Tigase's unexpected version=1.0 when ejabberd doesn't announce it (EJAB-1379)

--- src/ejabberd_s2s_out.erl
+++ src/ejabberd_s2s_out.erl
@@ -333,6 +333,10 @@ wait_for_stream({xmlstreamstart, Opening}, StateData) ->
 	{?NS_JABBER_SERVER, true, true} when
 	StateData#state.use_v10 ->
 	    {next_state, wait_for_features, StateData, ?FSMTIMEOUT};
+	%% Clause added to handle Tigase's workaround for an old ejabberd bug:
+	{?NS_JABBER_SERVER, true, true} when
+	not StateData#state.use_v10 ->
+	    send_db_request(StateData);
 	{?NS_JABBER_SERVER, false, true} when StateData#state.use_v10 ->
 	    {next_state, wait_for_features, StateData#state{db_enabled = false}, ?FSMTIMEOUT};
 	{NSProvided, DB, _} ->
