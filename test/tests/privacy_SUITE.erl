df5e32ef5580bfb0425523dd736eafa22503e17c
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2011-07-15 17:08:18 +0200

    Added privacy test suite

---
+++ test/tests/privacy_SUITE.erl
@@ -0,0 +1,84 @@
+%%==============================================================================
+%% Copyright 2010 Erlang Solutions Ltd.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%==============================================================================
+
+-module(privacy_SUITE).
+-compile(export_all).
+
+-include_lib("escalus/include/escalus.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% Suite configuration
+%%--------------------------------------------------------------------
+
+all() ->
+    [{group, privacy}].
+
+groups() ->
+    [{privacy, [sequence], [get_privacy_lists]}].
+
+suite() ->
+    escalus:suite().
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+init_per_suite(Config) ->
+    escalus:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    escalus:end_per_suite(Config).
+
+init_per_group(_GroupName, Config) ->
+    escalus:create_users(Config).
+
+end_per_group(_GroupName, Config) ->
+    escalus:delete_users(Config).
+
+init_per_testcase(CaseName, Config) ->
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
+
+%%--------------------------------------------------------------------
+%% Tests
+%%--------------------------------------------------------------------
+
+%% template
+%get_roster(Config) ->                                                                                                    
+    %escalus:story(Config, [1, 1], fun(Alice,_Bob) ->
+
+        %escalus_client:send(Alice, escalus_stanza:roster_get()),
+        %escalus_assert:is_roster_result(escalus_client:wait_for_stanza(Alice))
+
+        %end).
+
+get_privacy_lists(Config) ->
+    escalus:story(Config, [1], fun(Alice) ->
+
+        escalus_client:send(Alice, escalus_stanza:privacy_query(Alice)),
+        escalus_assert:is_privacy_query_result(
+            escalus_client:wait_for_stanza(Alice) )
+
+        end).
+
+%%-----------------------------------------------------------------
+%% Helpers
+%%-----------------------------------------------------------------
+
+% here
