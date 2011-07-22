a4e95db9e498fbb31067bd831236eb760531b8fb
Aleksandra Lipiec <aleksandra.lipiec@erlang-solutions.com>
2011-07-22 14:29:58 +0200

    Filling in roster and passwd mnesia tables

--- tools/fill_roster.erl
+++ tools/fill_roster.erl
@@ -4,27 +4,27 @@
 
 -module(fill_roster).
 
--export([fill/3]).
+-export([fill/4]).
 
 -include_lib("mod_roster.hrl").
 
-fill(UserNum, PerRosterNum, Type) when UserNum < PerRosterNum ->
-    fill_bucket(1, UserNum, Type);
-fill(UserNum, PerRosterNum, Type) ->
+fill(UserNum, PerRosterNum, Name, Type) when UserNum < PerRosterNum ->
+    fill_bucket(1, UserNum, Name, Type);
+fill(UserNum, PerRosterNum, Name, Type) ->
     UserNum2 = UserNum - PerRosterNum,
-    fill_bucket(UserNum2 + 1, UserNum, Type),
-    fill(UserNum2, PerRosterNum, Type).
+    fill_bucket(UserNum2 + 1, UserNum, Name, Type),
+    fill(UserNum2, PerRosterNum, Name, Type).
 
 %% [From, To]
-fill_bucket(From, To, Type) ->
+fill_bucket(From, To, Name, Type) ->
     lists:foreach(fun(UserN) ->
-                          add_contacts(UserN, From, To, Type)
+                          add_contacts(UserN, From, To, Name, Type)
                   end, lists:seq(From, To)).
 
-add_contacts(UserN, From, To, Type) ->
+add_contacts(UserN, From, To, Name, Type) ->
     lists:foreach(fun(ContactN) ->
-                          User = prep_el("user" ++ integer_to_list(UserN), Type),
-                          Contact = prep_el("user" ++ integer_to_list(ContactN), Type),
+                          User = prep_el(Name ++ integer_to_list(UserN), Type),
+                          Contact = prep_el(Name ++ integer_to_list(ContactN), Type),
                           Host = prep_el("localhost", Type),
                           Item = #roster{
                                usj = {User, Host, {Contact, Host, prep_el("", Type)}},
