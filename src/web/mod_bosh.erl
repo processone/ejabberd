006da5589e3bb143b7cdf068a08a36fd8345bcfb
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2011-07-26 16:24:21 +1000

    Fix copyright header

--- src/web/mod_bosh.erl
+++ src/web/mod_bosh.erl
@@ -1,14 +1,30 @@
 %%%-------------------------------------------------------------------
-%%% @author Evgeniy Khramtsov <ekhramtsov@process-one.net>
-%%% @copyright (C) 2011, Evgeniy Khramtsov
-%%% @doc
-%%% This module acts as a bridge to ejabberd_bosh which implements
-%%% the real stuff, this is to handle the new pluggable architecture for
-%%% extending ejabberd's http service.
-%%% @end
+%%% File    : mod_bosh.erl
+%%% Author  : Evgeniy Khramtsov <ekhramtsov@process-one.net>
+%%% Purpose : This module acts as a bridge to ejabberd_bosh which implements
+%%%           the real stuff, this is to handle the new pluggable architecture
+%%%           for extending ejabberd's http service.
 %%% Created : 20 Jul 2011 by Evgeniy Khramtsov <ekhramtsov@process-one.net>
+%%%
+%%%
+%%% ejabberd, Copyright (C) 2002-2011   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
 %%%-------------------------------------------------------------------
-
 -module(mod_bosh).
 -author('steve@zeank.in-berlin.de').
 
