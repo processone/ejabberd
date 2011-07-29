3ce736af2bed282e06e43e0a6a5d9b7e9a9d9f8e
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2011-07-29 10:41:16 +0200

    More stubs: mod_snmp_core.erl, mod_snmp.hrl

---
+++ apps/ejabberd/include/mod_snmp.hrl
@@ -0,0 +1,1 @@
+-define(STATS(asd), body).
--- apps/ejabberd/src/mod_snmp.erl
+++ apps/ejabberd/src/mod_snmp.erl
@@ -2,22 +2,40 @@
 
 -behaviour(gen_mod).
 
--export([start/2,
-         stop/1,
-         handle_entry/1]).
-
 -include("ejabberd.hrl").
 -include("jlib.hrl").
 -include("EJABBERD-MIB.hrl").
+-include("mod_snmp.hrl").
+
+%% behaviour: gen_mod
+-export([start/2, stop/1]).
+
+%% snmp handlers
+%-export([handle_entry/2, handle_entry/4]).
 
 
-start(_Host, _Opts) ->
+start(_Host, Opts) ->
     SampleOID = lists:foldl(fun(E,A) -> A ++ "." ++ integer_to_list(E) end,
         integer_to_list(hd(?ejabberd)), tl(?ejabberd)),
-    ?INFO_MSG("mod_snmp started. Sample OID is: ~s", [SampleOID]).
+    %% These are the options from ejabberd.cfg
+    SampleOpts = case Opts of
+            [H|T] ->
+                lists:foldl(fun(E,A) -> A ++ " " ++ atom_to_list(E) end,
+                    atom_to_list(H), T);
+            _ -> "none"
+        end,
+    ?INFO_MSG("mod_snmp started. Sample OID is: ~s~nSample opts: ~s", [SampleOID, SampleOpts]).
 
 stop(_Host) ->
     ok.
 
-handle_entry(_Args) ->
-    ok.
+%% The following is straight from ooVoo
+
+%-spec(handle_entry/2 :: (get | new | delete, atom()) -> {value, term()} | ok).
+%handle_entry(T, generalUptime)      -> ?UTILS:fun_value(T, getFun(generalUptime));
+%%% Default for counters
+%handle_entry(T, Counter) -> ?UTILS:counter_value(T, Counter).
+
+%%% Tables
+%-spec handle_entry(atom(), integer(), integer(), atom()) -> term().
+%handle_entry(T, Row, Col, Table) -> ?UTILS:table(T, Row, Col, getTableData(Table)).
