127b8b4603a999d7bc55046672acd176b781bad7
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2011-08-02 10:57:08 +0200

    Some SNMP tests and hooks (mod_privacy)

--- apps/ejabberd/src/ejabberd_snmp_hooks.erl
+++ apps/ejabberd/src/ejabberd_snmp_hooks.erl
@@ -8,18 +8,21 @@
 %%%------------------------------------------------------------------------
 -module(ejabberd_snmp_hooks).
 
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
 -export([get_hooks/1]).
 
 %%-------------------
-%% Internal eksports
+%% Internal exports
 %%-------------------
 -export([sm_register_connection_hook/3,
          sm_remove_connection_hook/3,
-         auth_failed/3]).
+         auth_failed/3,
+         privacy_iq_get/5,
+         privacy_iq_set/4]).
 
-
--include("ejabberd.hrl").
-
+-define(CORE, ejabberd_snmp_core).
 
 %%-------------------
 %% Implementation
@@ -29,8 +32,9 @@
 get_hooks(Host) ->
     [[sm_register_connection_hook, Host, ?MODULE, sm_register_connection_hook, 50],
      [sm_remove_connection_hook, Host, ?MODULE, sm_remove_connection_hook, 50],
-     [auth_failed, Host, ?MODULE, auth_failed, 50]].
-
+     [auth_failed, Host, ?MODULE, auth_failed, 50],
+     [privacy_iq_get, Host, ?MODULE, privacy_iq_get, 1],
+     [privacy_iq_set, Host, ?MODULE, privacy_iq_set, 1]].
 
 %%------------------------------
 %% SNMP specific hook callbacks
@@ -50,6 +54,23 @@ sm_remove_connection_hook(_,_,_) ->
 auth_failed(_,_,_) ->
     ejabberd_snmp_core:increment_counter(sessionAuthFails).
 
+-spec privacy_iq_get(term(), term(), term(), term(), term()) -> term().                                                        
+privacy_iq_get(Acc, _, _, _, _) ->
+    ?CORE:increment_counter(modPrivacyGets),
+    Acc.
 
+-spec privacy_iq_set(term(), term(), term(), term()) -> term().                                                        
+privacy_iq_set(Acc, _, _, #iq{sub_el = SubEl}) ->
+    {xmlelement, _, _, Els} = SubEl,
+    case xml:remove_cdata(Els) of
+	[{xmlelement, <<"active">>, _, _}] ->
+            ?CORE:increment_counter(modPrivacySetsActive);
+	[{xmlelement, <<"default">>, _, _}] ->
+            ?CORE:increment_counter(modPrivacySetsDefault);
+    _ ->
+        ok
+    end,
+    ?CORE:increment_counter(modPrivacySets),
+    Acc.
 
-
+%%% vim: set sts=4 ts=4 sw=4 et filetype=erlang foldmarker=%%%',%%%. foldmethod=marker:
---
+++ test/tests/snmp_SUITE.erl
@@ -0,0 +1,146 @@
+%%==============================================================================
+%% Copyright 2010 Erlang Solutions Ltd.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%==============================================================================
+
+-module(snmp_SUITE).
+-compile(export_all).
+
+-include_lib("escalus/deps/exmpp/include/exmpp.hrl").
+-include_lib("escalus/include/escalus.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+-define(RPC(Mod, Fun, Args), escalus_ejabberd:rpc(Mod, Fun, Args)).
+-define(RPC_LOOKUP(Table, Counter), escalus_ejabberd:rpc(ets, lookup, [Table, Counter])).
+
+-import(privacy_helper, [config_list/2,
+                         set_and_activate/2,
+                         set_list/2,
+                         activate_list/2,
+                         privacy_list/2,
+                         privacy_list_item/1,
+                         is_privacy_list_push/1,
+                         is_presence_error/1,
+                         verify_result/1,
+                         verify_push/1,
+                         verify_presence_error/1]).
+
+%%--------------------------------------------------------------------
+%% Suite configuration
+%%--------------------------------------------------------------------
+
+all() ->
+    [{group, core},
+     {group, general},
+     {group, c2s},
+     {group, mod_privacy},
+     {group, mod_roster},
+     {group, mod_register}].
+
+groups() ->
+    [{core, [sequence], [generalUptime,
+                         generalNodeName]},
+     {mod_privacy, [sequence], [modPrivacyGets,
+                                modPrivacySets,
+                                modPrivacySetsActive]}].
+
+suite() ->
+    [{require, privacy_lists} | escalus:suite()].
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+init_per_suite(Config) ->
+    PrivacyLists = lists:map(
+        fun(Name) ->
+            { Name,
+              privacy_list(Name,
+                  [privacy_list_item(Item) || Item <- ct:get_config(Name)]) }
+        end,
+        ct:get_config(privacy_lists)),
+    escalus:init_per_suite(PrivacyLists ++ Config).
+
+end_per_suite(Config) ->
+    escalus:end_per_suite(Config).
+
+init_per_group(_GroupName, Config) ->
+    escalus:create_users(Config).
+
+end_per_group(_GroupName, Config) ->
+    escalus:delete_users(Config).
+
+init_per_testcase(CaseName, Config) ->
+    escalus_ejabberd:rpc(ejabberd_snmp_core, reset_counters, []),
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
+
+%%--------------------------------------------------------------------
+%% Tests
+%%--------------------------------------------------------------------
+
+modPrivacyGets(Config) ->
+    escalus:story(Config, [1], fun(Alice) ->
+
+        Table = stats_mod_privacy,
+        Counter = modPrivacyGets,
+
+        [{Counter, 0}] = ?RPC_LOOKUP(Table, Counter),
+        escalus_client:send(Alice, escalus_stanza:privacy_get_all(Alice)),
+        escalus_client:wait_for_stanzas(Alice, 1),
+        [{Counter, 1}] = ?RPC_LOOKUP(Table, Counter)
+
+        end).
+
+modPrivacySets() -> [{require, alice_deny_bob}].
+
+modPrivacySets(Config) ->
+    escalus:story(Config, [1], fun(Alice) ->
+
+        PrivacyList = config_list(alice_deny_bob, Config),
+
+        Table = stats_mod_privacy,
+        Counter = modPrivacySets,
+
+        [{Counter, 0}] = ?RPC_LOOKUP(Table, Counter),
+        set_list(Alice, PrivacyList),
+        [{Counter, 1}] = ?RPC_LOOKUP(Table, Counter)
+
+        end).
+
+modPrivacySetsActive() -> [{require, alice_deny_bob}].
+
+modPrivacySetsActive(Config) ->
+    escalus:story(Config, [1], fun(Alice) ->
+
+        PrivacyList = config_list(alice_deny_bob, Config),
+
+        Table = stats_mod_privacy,
+        Counter = modPrivacySetsActive,
+
+        [{Counter, 0}] = ?RPC_LOOKUP(Table, Counter),
+        set_list(Alice, PrivacyList),
+        [{Counter, 0}] = ?RPC_LOOKUP(Table, Counter),
+        activate_list(Alice, PrivacyList),
+        [{Counter, 1}] = ?RPC_LOOKUP(Table, Counter)
+
+        end).
+
+%%-----------------------------------------------------------------
+%% Helpers
+%%-----------------------------------------------------------------
+
+
