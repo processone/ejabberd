8b5fa88b6f326ef5334ee5ae53839143786e8374
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2011-09-05 16:52:36 +1000

    Export do_client_version/3 in order to shut up the dialyzer

--- src/mod_echo.erl
+++ src/mod_echo.erl
@@ -31,7 +31,7 @@
 -behaviour(gen_mod).
 
 %% API
--export([start_link/2, start/2, stop/1]).
+-export([start_link/2, start/2, stop/1, do_client_version/3]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
