d5b4d6785879f0a5192c26f5b5e218aec8104798
Christophe Romain <christophe.romain@process-one.net>
2011-09-21 14:39:50 +0200

    Fix Denial of Service when user sends malformed publish stanza (thanks to Oleg Smirnov) (EJAB-1498)

--- src/mod_pubsub/mod_pubsub_odbc.erl
+++ src/mod_pubsub/mod_pubsub_odbc.erl
@@ -1921,8 +1921,10 @@ publish_item(Host, ServerHost, Node, Publisher, ItemId, Payload) ->
 	    case lists:member("auto-create", features(Type)) of
 		true ->
 		    case create_node(Host, ServerHost, Node, Publisher, Type) of
-			{result, _} ->
-			    publish_item(Host, ServerHost, Node, Publisher, ItemId, Payload);
+			{result, [{xmlelement, "pubsub", [{"xmlns", ?NS_PUBSUB}],
+			  [{xmlelement, "create", [{"node", NewNode}], []}]}]} ->
+			    publish_item(Host, ServerHost,  list_to_binary(NewNode),
+				    Publisher, ItemId, Payload);
 			_ ->
 			    {error, ?ERR_ITEM_NOT_FOUND}
 		    end;
