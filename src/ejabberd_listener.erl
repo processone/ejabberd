3a51c801e1cf639fdbf5af2fe12b3eb39b7c34ff
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2012-03-19 18:52:20 +1000

    Change log level from informational to error

--- src/ejabberd_listener.erl
+++ src/ejabberd_listener.erl
@@ -290,8 +290,8 @@ accept(ListenSocket, Module, Opts) ->
 	    CallMod:start(strip_frontend(Module), gen_tcp, Socket, Opts),
 	    accept(ListenSocket, Module, Opts);
 	{error, Reason} ->
-	    ?INFO_MSG("(~w) Failed TCP accept: ~w",
-		      [ListenSocket, Reason]),
+	    ?ERROR_MSG("(~w) Failed TCP accept: ~w",
+                       [ListenSocket, Reason]),
 	    accept(ListenSocket, Module, Opts)
     end.
 
