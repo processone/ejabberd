362d7f617d3ea46ee3519356c0a7227ba293b747
Alexey Shchepin <alexey@process-one.net>
2012-12-20 14:01:45 +0200

    Fixed signedness issue in tls_drv GET_DESCRYPTED_INPUT (EJAB-1591)

--- src/tls/tls_drv.c
+++ src/tls/tls_drv.c
@@ -536,8 +536,9 @@ static ErlDrvSSizeT tls_drv_control(ErlDrvData handle,
 	    size_t req_size = 0;
 	    if (len == 4)
 	    {
+	       unsigned char *b = (unsigned char *)buf;
 	       req_size =
-		  (buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | buf[3];
+		  (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
 	    }
 	    size = BUF_SIZE + 1;
 	    rlen = 1;
