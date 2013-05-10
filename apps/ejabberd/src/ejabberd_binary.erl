402e1ff8acaea25dc7d23e14342a2e7053a218c8
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2013-05-10 17:52:34 +0200

    Allow to inspect the backtrace on conversion failure
    Catching the exception will most probably cause a badmatch and show a
    backtrace. Otherwise the information where a badarg happens is hidden,
    because the reported location is always inside `ejabberd_binary` module.

--- apps/ejabberd/src/ejabberd_binary.erl
+++ apps/ejabberd/src/ejabberd_binary.erl
@@ -6,9 +6,9 @@
          integer_to_binary/1]).
 
 binary_to_integer(B) ->
-    list_to_integer(binary_to_list(B)).
+    catch list_to_integer(binary_to_list(B)).
 
 integer_to_binary(I) ->
-    list_to_binary(integer_to_list(I)).
+    catch list_to_binary(integer_to_list(I)).
 
 -endif.
