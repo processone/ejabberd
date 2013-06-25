ae1af885ce7e485f7562e051e0014371d7c10629
Christophe Romain <christophe.romain@process-one.net>
2013-06-25 14:49:22 +0200

    fix feature build on node disco#info

--- src/mod_pubsub.erl
+++ src/mod_pubsub.erl
@@ -1671,7 +1671,7 @@ node_disco_info(Host, Node, From, Identity, Features) ->
 %	Other -> Other
 %    end.
     Action = fun (#pubsub_node{type = Type, id = NodeId}) ->
-		     I = Types = case tree_call(Host, get_subnodes,
+		     Types = case tree_call(Host, get_subnodes,
 						      [Host, Node, From])
 					   of
 					 [] -> [<<"leaf">>];
@@ -1687,7 +1687,7 @@ node_disco_info(Host, Node, From, Identity, Features) ->
 					       _ -> []
 					     end
 				       end,
-			       lists:map(fun (T) ->
+		    I = lists:map(fun (T) ->
 						 #xmlel{name = <<"identity">>,
 							attrs =
 							    [{<<"category">>,
