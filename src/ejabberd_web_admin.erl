c29ba14dbfb902e3096d02ed77c646ead53cd73f
Badlop <badlop@process-one.net>
2014-02-26 17:18:12 +0100

    Don't provide current password in webinterface (github issue #137)

--- src/ejabberd_web_admin.erl
+++ src/ejabberd_web_admin.erl
@@ -1570,9 +1570,7 @@ user_info(User, Server, Query, Lang) ->
                         end,
                         lists:sort(Resources))))]
         end,
-    Password = ejabberd_auth:get_password_s(User, Server),
-    FPassword = [?INPUT(<<"password">>, <<"password">>,
-			Password),
+    FPassword = [?INPUT(<<"text">>, <<"password">>, <<"">>),
 		 ?C(<<" ">>),
 		 ?INPUTT(<<"submit">>, <<"chpassword">>,
 			 <<"Change Password">>)],
