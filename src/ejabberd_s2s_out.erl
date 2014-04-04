96af0ab3d83244b7f40c31547af05b068971295c
Antonio Murdaca <antonio.murdaca@hadlab.com>
2014-04-05 01:07:04 +0200

    Add better defined configuration options

--- src/ejabberd_s2s_out.erl
+++ src/ejabberd_s2s_out.erl
@@ -197,7 +197,14 @@ init([From, Server, Type]) ->
                    Ciphers -> [{ciphers, Ciphers} | TLSOpts1]
                end,
     TLSOpts3 = case ejabberd_config:get_option(
-                      s2s_protocol_options, fun iolist_to_binary/1) of
+                      s2s_protocol_options,
+                      fun (Options) ->
+                              [_|Opts] = lists:foldl(
+                                           fun(X, Acc) -> X ++ Acc end, [],
+                                           [["|" | Opt] || Opt <- Options, is_list(Opt)]
+                                          ),
+                              iolist_to_binary(Opts)
+                      end) of
                    undefined -> TLSOpts2;
                    ProtocolOpts -> [{protocol_options, ProtocolOpts} | TLSOpts2]
                end,
