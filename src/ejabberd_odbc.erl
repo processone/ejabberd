a6408e92818a52f92c47a70ffd4eecb127ebc7cc
HAMANO Tsukasa <hamano@cuspy.org>
2014-04-30 15:32:07 +0900

    fix error handling when sql calling with unknown host.

--- src/ejabberd_odbc.erl
+++ src/ejabberd_odbc.erl
@@ -141,7 +141,7 @@ sql_call(Host, Msg) ->
     case get(?STATE_KEY) of
       undefined ->
         case ejabberd_odbc_sup:get_random_pid(Host) of
-          none -> ?WARNING_MSG("SQL calling unknown host: ~p~n", [Host]);
+          none -> {error, unknownhost};
           Pid ->
             (?GEN_FSM):sync_send_event(Pid,{sql_cmd, Msg, now()},
                                        ?TRANSACTION_TIMEOUT)
