a6ddab1e9dd448e8c73b41f0208f6ecd65e7283d
Badlop <badlop@process-one.net>
2014-05-14 13:28:39 +0200

    Fix bug when joining empty path

--- src/mod_muc_log.erl
+++ src/mod_muc_log.erl
@@ -1240,5 +1240,6 @@ calc_hour_offset(TimeHere) ->
 	  3600,
     TimeHereHour - TimeZeroHour.
 
+fjoin([]) -> <<"/">>;
 fjoin(FileList) ->
     list_to_binary(filename:join([binary_to_list(File) || File <- FileList])).
