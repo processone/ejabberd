2c4647a98070dc6fc27d25b1d58e50ac6d028c54
Badlop <badlop@process-one.net>
2015-01-15 20:00:06 +0100

    Fix binaries when ejabberd_ctl passes auth details

--- src/ejabberd_ctl.erl
+++ src/ejabberd_ctl.erl
@@ -210,7 +210,7 @@ process(Args) ->
 
 %% @spec (Args::[string()], AccessCommands) -> {String::string(), Code::integer()}
 process2(["--auth", User, Server, Pass | Args], AccessCommands) ->
-    process2(Args, {User, Server, Pass}, AccessCommands);
+    process2(Args, {list_to_binary(User), list_to_binary(Server), list_to_binary(Pass)}, AccessCommands);
 process2(Args, AccessCommands) ->
     process2(Args, noauth, AccessCommands).
 
