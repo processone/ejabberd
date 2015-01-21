120e581865671c1939234492054cca88fb5aba28
Christophe Romain <christophe.romain@process-one.net>
2015-01-21 14:52:37 +0100

    Fix missing copyright dates to 2015 (EJAB-1733)

--- src/ejabberd_auth_riak.erl
+++ src/ejabberd_auth_riak.erl
@@ -5,7 +5,7 @@
 %%% Created : 12 Nov 2012 by Evgeniy Khramtsov <ekhramtsov@process-one.net>
 %%%
 %%%
-%%% ejabberd, Copyright (C) 2002-2012   ProcessOne
+%%% ejabberd, Copyright (C) 2002-2015   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
--- src/ejabberd_riak_sup.erl
+++ src/ejabberd_riak_sup.erl
@@ -5,7 +5,7 @@
 %%% Created : 29 Dec 2011 by Alexey Shchepin <alexey@process-one.net>
 %%%
 %%%
-%%% ejabberd, Copyright (C) 2002-2011   ProcessOne
+%%% ejabberd, Copyright (C) 2002-2015   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
