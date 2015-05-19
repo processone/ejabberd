3b267eaa61c3659e51be69aac47ef2efbf1f8958
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2015-05-19 10:51:49 +0300

    Do not bounce normal messages sent to unavailable resource

--- src/ejabberd_sm.erl
+++ src/ejabberd_sm.erl
@@ -539,6 +539,8 @@ do_route(From, To, #xmlel{} = Packet) ->
 		  <<"message">> ->
 		      case xml:get_attr_s(<<"type">>, Attrs) of
 			<<"chat">> -> route_message(From, To, Packet, chat);
+			<<"normal">> -> route_message(From, To, Packet, normal);
+			<<"">> -> route_message(From, To, Packet, normal);
 			<<"error">> -> ok;
 			_ ->
 			    Err = jlib:make_error_reply(Packet,
