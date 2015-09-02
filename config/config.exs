06193d18b89c0d8a48dda757b7e2b68dd9ce4eda
Mickael Remond <mremond@process-one.net>
2015-09-02 17:02:09 +0200

    Better to keep config file in a different config directory

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # This is standard path in the context of ejabberd release
 config :ejabberd,
-  file: "running-config/ejabberd.yml",
+  file: "config/ejabberd.yml",
   log_path: 'log/ejabberd.log'
  
 # Customize Mnesia directory:
