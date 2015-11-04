fec7e47169142d428cb6f779552b6bf2e1dea91b
Holger Weiss <holger@zedat.fu-berlin.de>
2015-11-04 01:22:39 +0100

    Add copyright and license to HTTP upload modules

--- src/mod_http_upload_quota.erl
+++ src/mod_http_upload_quota.erl
@@ -1,9 +1,27 @@
-%%%-------------------------------------------------------------------
+%%%----------------------------------------------------------------------
 %%% File    : mod_http_upload_quota.erl
 %%% Author  : Holger Weiss <holger@zedat.fu-berlin.de>
 %%% Purpose : Quota management for HTTP File Upload (XEP-0363)
 %%% Created : 15 Oct 2015 by Holger Weiss <holger@zedat.fu-berlin.de>
-%%%-------------------------------------------------------------------
+%%%
+%%%
+%%% ejabberd, Copyright (C) 2015   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License along
+%%% with this program; if not, write to the Free Software Foundation, Inc.,
+%%% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%%%
+%%%----------------------------------------------------------------------
 
 -module(mod_http_upload_quota).
 -author('holger@zedat.fu-berlin.de').
