325c17f277fa8c4f5c33a6e26acbfcd9d25197f9
Holger Weiss <holger@zedat.fu-berlin.de>
2015-12-08 22:21:39 +0100

    mod_mam: Update record field type

--- src/mod_mam.erl
+++ src/mod_mam.erl
@@ -46,7 +46,7 @@
 	{us = {<<"">>, <<"">>}                :: {binary(), binary()} | '$2',
 	 id = <<>>                            :: binary() | '_',
 	 timestamp = p1_time_compat:timestamp() :: erlang:timestamp() | '_' | '$1',
-	 peer = {<<"">>, <<"">>, <<"">>}      :: ljid() | '_' | '$3',
+	 peer = {<<"">>, <<"">>, <<"">>}      :: ljid() | '_' | '$3' | undefined,
 	 bare_peer = {<<"">>, <<"">>, <<"">>} :: ljid() | '_' | '$3',
 	 packet = #xmlel{}                    :: xmlel() | '_',
 	 nick = <<"">>                        :: binary(),
