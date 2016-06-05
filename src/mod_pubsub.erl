78d4200f05ca513cc873b4e89fd2c50f27c571b1
Holger Weiss <holger@zedat.fu-berlin.de>
2016-06-06 00:18:24 +0200

    mod_pubsub: Fix node configuration changes for SQL
    nodetree_tree_sql:set_node/1 returns {ok, NodeIdx} rather than 'ok' on
    success.

--- src/mod_pubsub.erl
+++ src/mod_pubsub.erl
@@ -3873,6 +3873,7 @@ set_configure(Host, Node, From, Els, Lang) ->
 							    set_node,
 							    [N#pubsub_node{options = NewOpts}])
 						    of
+							{ok, Nidx} -> {result, ok};
 							ok -> {result, ok};
 							Err -> Err
 						    end;
