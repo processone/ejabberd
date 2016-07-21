b4bb471301bd59714c7b26cb4f4baed73aea3c0b
gabrielgatu <gabriel.dny@gmail.com>
2016-07-21 10:56:13 +0200

    Resolve issues when using elixir config with built ejabberd

--- lib/ejabberd/config_util.ex
+++ lib/ejabberd/config_util.ex
@@ -1,5 +1,9 @@
 defmodule Ejabberd.ConfigUtil do
 
+  def is_elixir_config(filename) when is_list(filename) do
+    is_elixir_config(to_string(filename))
+  end
+
   def is_elixir_config(filename) do
     String.ends_with?(filename, "exs")
   end
