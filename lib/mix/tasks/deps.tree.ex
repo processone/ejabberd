bb9c3cd0a03a67026f5afa3196b152e53b20f240
gabrielgatu <gabriel.dny@gmail.com>
2016-07-22 12:29:40 +0200

    Fix call to Ejabberd.Config.init/1 in deps.tree

--- lib/mix/tasks/deps.tree.ex
+++ lib/mix/tasks/deps.tree.ex
@@ -14,7 +14,7 @@ defmodule Mix.Tasks.Ejabberd.Deps.Tree do
     # First we need to start manually the store to be available
     # during the compilation of the config file.
     Ejabberd.Config.Store.start_link
-    Ejabberd.Config.init
+    Ejabberd.Config.init(:ejabberd_config.get_ejabberd_config_path())
 
     Mix.shell.info "ejabberd modules"
 
