57aeef74d5f572438d60a0c8e7a4f9c9933d3852
Pablo Polvorin <pablo@work>
2016-07-22 19:15:56 -0300

    stringprep might already be started
    Depending on the way the test us ran
    (full test suite or the elixir quicktest one)
    the stringprep might already be loaded.

--- test/acl_test.exs
+++ test/acl_test.exs
@@ -26,7 +26,7 @@ defmodule ACLTest do
   setup_all do
     :ok = :mnesia.start
     :ok = :jid.start
-    :ok = :stringprep.start
+    :stringprep.start
     :ok = :ejabberd_config.start(["domain1", "domain2"], [])
     :ok = :acl.start
   end
