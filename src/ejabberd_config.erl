9e07c16090f443c146c8284ab47cc673cc71715b
gabrielgatu <gabriel.dny@gmail.com>
2016-07-22 12:38:28 +0200

    Move is_using_elixir_config/0 inside ejabberd_config.erl

--- src/ejabberd_config.erl
+++ src/ejabberd_config.erl
@@ -33,7 +33,7 @@
          get_option/2, get_option/3, add_option/2, has_option/1,
          get_vh_by_auth_method/1, is_file_readable/1,
          get_version/0, get_myhosts/0, get_mylang/0,
-         get_ejabberd_config_path/0,
+         get_ejabberd_config_path/0, is_using_elixir_config/0,
          prepare_opt_val/4, convert_table_to_binary/5,
          transform_options/1, collect_options/1, default_db/2,
          convert_to_yaml/1, convert_to_yaml/2, v_db/2,
@@ -1046,6 +1046,10 @@ replace_modules(Modules) ->
 
 %% Elixir module naming
 %% ====================
+
+is_using_elixir_config() ->
+  Config = get_ejabberd_config_path(),
+  'Elixir.Ejabberd.ConfigUtil':is_elixir_config(Config).
 
 %% If module name start with uppercase letter, this is an Elixir module:
 is_elixir_module(Module) ->
