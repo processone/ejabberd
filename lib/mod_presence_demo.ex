76eba3647a9763e7e1e65759f13f61d3b9e73e87
Mickael Remond <mremond@process-one.net>
2016-08-01 15:46:14 +0200

    Implement gen_mod callback in ModPresenceDemo module

--- lib/mod_presence_demo.ex
+++ lib/mod_presence_demo.ex
@@ -7,15 +7,20 @@ defmodule ModPresenceDemo do
     Ejabberd.Hooks.add(:set_presence_hook, host, __ENV__.module, :on_presence, 50)
     :ok
   end
-  
+
   def stop(host) do
     info('Stopping ejabberd module Presence Demo')
     Ejabberd.Hooks.delete(:set_presence_hook, host, __ENV__.module, :on_presence, 50)
     :ok
   end
-  
+
   def on_presence(user, _server, _resource, _packet) do
     info('Receive presence for #{user}')
     :none
-  end 
+  end
+
+  # gen_mod callbacks
+  def depends(_host, _opts), do: []
+  def mod_opt_type(_), do: []
+
 end
