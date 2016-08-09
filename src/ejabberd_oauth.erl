50596dc4d347fc76f01f7b063b42c85296fad55b
Paweł Chmielowski <pchmielowski@process-one.net>
2016-08-09 19:22:51 +0200

    Provide proper args_desc in oauth_issue_token command

--- src/ejabberd_oauth.erl
+++ src/ejabberd_oauth.erl
@@ -102,7 +102,9 @@ get_commands_spec() ->
                         args = [{jid, string},{ttl, integer}, {scopes, string}],
                         policy = restricted,
                         args_example = ["user@server.com", "connected_users_number;muc_online_rooms"],
-                        args_desc = ["List of scopes to allow, separated by ';'"],
+                        args_desc = ["Jid for which issue token",
+				     "Time to live of generated token in seconds",
+				     "List of scopes to allow, separated by ';'"],
                         result = {result, {tuple, [{token, string}, {scopes, string}, {expires_in, string}]}}
                        },
      #ejabberd_commands{name = oauth_list_tokens, tags = [oauth],
