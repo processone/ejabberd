57a44a7eb15df49bce8e45b27aa935e96961c2d0
gabrielgatu <gabriel.dny@gmail.com>
2016-08-12 13:40:01 +0200

    Add case for :attr_not_supported in EjabberdLogger.log_errors/1

--- lib/ejabberd/config/logger/ejabberd_logger.ex
+++ lib/ejabberd/config/logger/ejabberd_logger.ex
@@ -4,13 +4,13 @@ defmodule Ejabberd.Config.EjabberdLogger do
   given validated modules.
   """
 
-  @spec log_errors([EjabberdModule.t]) :: [EjabberdModule.t]
   alias Ejabberd.Config.{EjabberdModule}
 
   @doc """
   Given a list of modules validated, in the form of {:ok, mod} or
   {:error, mod, errors}, it logs to the user the errors found.
   """
+  @spec log_errors([EjabberdModule.t]) :: [EjabberdModule.t]
   def log_errors(modules_validated) when is_list(modules_validated) do
     Enum.each modules_validated, &do_log_errors/1
     modules_validated
@@ -21,8 +21,11 @@ defmodule Ejabberd.Config.EjabberdLogger do
   defp do_log_errors({:attribute, errors}), do: Enum.each errors, &log_attribute_error/1
   defp do_log_errors({:dependency, errors}), do: Enum.each errors, &log_dependency_error/1
 
+  defp log_attribute_error({{attr_name, val}, :attr_not_supported}), do:
+    IO.puts "[ WARN ] Annotation @#{attr_name} is not supported."
+
   defp log_attribute_error({{attr_name, val}, :type_not_supported}), do:
-    IO.puts "[ WARN ] Annotation @#{inspect attr_name} with value #{inspect val} is not supported."
+    IO.puts "[ WARN ] Annotation @#{attr_name} with value #{inspect val} is not supported (type mismatch)."
 
   defp log_dependency_error({module, :not_found}), do:
     IO.puts "[ WARN ] Module #{inspect module} was not found, but is required as a dependency."
