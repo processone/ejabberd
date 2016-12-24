7ef8dfb4e0d61f34ce07e7b65edcbe0cb164d2f5
Holger Weiss <holger@zedat.fu-berlin.de>
2016-12-25 00:58:20 +0100

    Fix 'nodetree' configuration lookups for PEP

--- src/mod_pubsub.erl
+++ src/mod_pubsub.erl
@@ -3432,7 +3432,7 @@ serverhost(Host) ->
 
 -spec tree(host()) -> atom().
 tree(Host) ->
-    case config(Host, nodetree) of
+    case config(serverhost(Host), nodetree) of
 	undefined -> tree(Host, ?STDTREE);
 	Tree -> Tree
     end.
