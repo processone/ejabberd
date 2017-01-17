c7476875e65ce648ea58364fecbc2097a2285da1
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2017-01-17 14:31:00 +0300

    Avoid stopping mod_offline before supervisor termination (#1464)

--- src/mod_offline.erl
+++ src/mod_offline.erl
@@ -122,7 +122,6 @@ start(Host, Opts) ->
 
 stop(Host) ->
     Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
-    catch ?GEN_SERVER:call(Proc, stop),
     supervisor:terminate_child(ejabberd_sup, Proc),
     supervisor:delete_child(ejabberd_sup, Proc),
     ok.
