61cdee97fc1f808741b5950c77b7e1d9ca5fd611
Holger Weiss <holger@zedat.fu-berlin.de>
2017-02-20 13:07:34 +0100

    ejabberd_c2s: Don't drop 'user_send_packet' result
    When handling a <session/> IQ, use the stanza returned by the
    'user_send_packet' hook.
    
    Thanks to Evgeniy for spotting this.

--- src/ejabberd_c2s.erl
+++ src/ejabberd_c2s.erl
@@ -449,11 +449,11 @@ handle_authenticated_packet(Pkt, #{lserver := LServer, jid := JID,
 	drop ->
 	    State2;
 	#iq{type = set, sub_els = [_]} ->
-	    case xmpp:get_subtag(Pkt1, #xmpp_session{}) of
+	    case xmpp:get_subtag(Pkt2, #xmpp_session{}) of
 		#xmpp_session{} ->
-		    send(State2, xmpp:make_iq_result(Pkt1));
+		    send(State2, xmpp:make_iq_result(Pkt2));
 		_ ->
-		    check_privacy_then_route(State2, Pkt1)
+		    check_privacy_then_route(State2, Pkt2)
 	    end;
 	#presence{to = #jid{luser = LUser, lserver = LServer,
 			    lresource = <<"">>}} ->
