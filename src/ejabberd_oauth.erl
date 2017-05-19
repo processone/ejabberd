654d907dcfed85403c4a45070e093287ffe54328
Paweł Chmielowski <pchmielowski@process-one.net>
2017-05-19 17:03:41 +0200

    export_all is not needed here

--- src/ejabberd_oauth.erl
+++ src/ejabberd_oauth.erl
@@ -29,8 +29,6 @@
 -behaviour(gen_server).
 -behaviour(ejabberd_config).
 
--compile(export_all).
-
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2,
 	 handle_info/2, terminate/2, code_change/3]).
