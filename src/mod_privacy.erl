f6bdc6fdb2ee4a6bc4323a82f647b8795acab2a2
Holger Weiss <holger@zedat.fu-berlin.de>
2017-07-07 14:28:22 +0200

    mod_privacy: Don't crash while copying c2s state
    Don't assume 'privacy_active_list' is set when c2s_copy_session/2 is
    called.

--- src/mod_privacy.erl
+++ src/mod_privacy.erl
@@ -412,7 +412,9 @@ decode_item(#privacy_item{order = Order,
 
 -spec c2s_copy_session(c2s_state(), c2s_state()) -> c2s_state().
 c2s_copy_session(State, #{privacy_active_list := List}) ->
-    State#{privacy_active_list => List}.
+    State#{privacy_active_list => List};
+c2s_copy_session(State, _) ->
+    State.
 
 -spec user_send_packet({stanza(), c2s_state()}) -> {stanza(), c2s_state()}.
 user_send_packet({#iq{type = Type,
