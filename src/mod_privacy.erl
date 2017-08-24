30bca124f452f42c1a5d2c0e1c7827bbf59c75b2
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2017-08-24 21:40:54 +0300

    Add forgotten caching options to the validator (thanks to Jan Pinkas)

--- src/mod_privacy.erl
+++ src/mod_privacy.erl
@@ -846,4 +846,12 @@ depends(_Host, _Opts) ->
 
 mod_opt_type(db_type) -> fun(T) -> ejabberd_config:v_db(?MODULE, T) end;
 mod_opt_type(iqdisc) -> fun gen_iq_handler:check_type/1;
-mod_opt_type(_) -> [db_type, iqdisc].
+mod_opt_type(O) when O == cache_life_time; O == cache_size ->
+    fun (I) when is_integer(I), I > 0 -> I;
+        (infinity) -> infinity
+    end;
+mod_opt_type(O) when O == use_cache; O == cache_missed ->
+    fun (B) when is_boolean(B) -> B end;
+mod_opt_type(_) ->
+    [db_type, iqdisc, cache_life_time, cache_size,
+     use_cache, cache_missed].
