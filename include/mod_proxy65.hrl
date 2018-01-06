150f33b39b8a4aa1fc6bba403e46f04c6dfbcfd0
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2018-01-06 14:01:45 +0300

    Update copyright dates in header files

--- include/mod_proxy65.hrl
+++ include/mod_proxy65.hrl
@@ -2,7 +2,7 @@
 %%% RFC 1928 constants.
 %%%
 %%%
-%%% ejabberd, Copyright (C) 2002-2017   ProcessOne
+%%% ejabberd, Copyright (C) 2002-2018   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
