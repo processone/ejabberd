processone/ejabberd

commit 4b012a99d2bdd6d22f05676e9a7989409e314fca

Introduce option 'captcha' for mod_block_strangers
When the option is set to `true`, the module will generate
CAPTCHA challenges for incoming subscription requests. The option
also implies that option `drop` is set to `true`. Note that
the module won't generate CAPTCHA challenges for messages: they
will still be rejected if `drop` is set to `true`.

Fixes #2246

src/mod_last.erl
@@ -142,9 +142,9 @@ process_sm_iq(#iq{type = set, lang = Lang} = IQ) ->
 process_sm_iq(#iq{from = From, to = To, lang = Lang} = IQ) ->
     User = To#jid.luser,
     Server = To#jid.lserver,
-    {Subscription, _Groups} =
+    {Subscription, _Ask, _Groups} =
 	ejabberd_hooks:run_fold(roster_get_jid_info, Server,
-				{none, []}, [User, Server, From]),
+				{none, none, []}, [User, Server, From]),
     if (Subscription == both) or (Subscription == from) or
        (From#jid.luser == To#jid.luser) and
        (From#jid.lserver == To#jid.lserver) ->

src/ejabberd_c2s.erl
@@ -643,8 +643,8 @@ route_probe_reply(_, _) ->
     ok.
 
 -spec process_presence_out(state(), presence()) -> state().
-process_presence_out(#{user := User, server := Server, lserver := LServer,
-		       jid := JID, lang := Lang, pres_a := PresA} = State,
+process_presence_out(#{lserver := LServer, jid := JID,
+		       lang := Lang, pres_a := PresA} = State,
 		     #presence{from = From, to = To, type = Type} = Pres) ->
     if Type == subscribe; Type == subscribed;
        Type == unsubscribe; Type == unsubscribed ->
@@ -656,9 +656,7 @@ process_presence_out(#{user := User, server := Server, lserver := LServer,
 		    AccessErr = xmpp:err_forbidden(AccessErrTxt, Lang),
 		    send_error(State, Pres, AccessErr);
 		allow ->
-		    ejabberd_hooks:run(roster_out_subscription,
-				       LServer,
-				       [User, Server, To, Type])
+		    ejabberd_hooks:run(roster_out_subscription, LServer, [Pres])
 	    end;
 	true -> ok
     end,
@@ -839,9 +837,9 @@ route_multiple(#{lserver := LServer}, JIDs, Pkt) ->
     ejabberd_router_multicast:route_multicast(From, LServer, JIDs, Pkt).
 
 get_subscription(#jid{luser = LUser, lserver = LServer}, JID) ->
-    {Subscription, _} = ejabberd_hooks:run_fold(
-			  roster_get_jid_info, LServer, {none, []},
-			  [LUser, LServer, JID]),
+    {Subscription, _, _} = ejabberd_hooks:run_fold(
+			     roster_get_jid_info, LServer, {none, none, []},
+			     [LUser, LServer, JID]),
     Subscription.
 
 -spec resource_conflict_action(binary(), binary(), binary()) ->

src/ejabberd_captcha.erl
@@ -41,7 +41,8 @@
 -export([create_captcha/6, build_captcha_html/2,
 	 check_captcha/2, process_reply/1, process/2,
 	 is_feature_available/0, create_captcha_x/5,
-	 opt_type/1]).
+	 opt_type/1, host_up/1, host_down/1,
+	 config_reloaded/0, process_iq/1]).
 
 -include("xmpp.hrl").
 -include("ejabberd.hrl").
@@ -53,7 +54,8 @@
 
 -type image_error() :: efbig | enodata | limit | malformed_image | timeout.
 
--record(state, {limits = treap:empty() :: treap:treap()}).
+-record(state, {limits = treap:empty() :: treap:treap(),
+		enabled = false :: boolean()}).
 
 -record(captcha, {id :: binary(),
                   pid :: pid() | undefined,
@@ -214,6 +216,25 @@ process_reply(#xcaptcha{xdata = #xdata{} = X}) ->
 process_reply(_) ->
     {error, malformed}.
 
+process_iq(#iq{type = set, lang = Lang, sub_els = [#xcaptcha{} = El]} = IQ) ->
+    case process_reply(El) of
+	ok ->
+	    xmpp:make_iq_result(IQ);
+	{error, malformed} ->
+	    Txt = <<"Incorrect CAPTCHA submit">>,
+	    xmpp:make_error(IQ, xmpp:err_bad_request(Txt, Lang));
+	{error, _} ->
+	    Txt = <<"The CAPTCHA verification has failed">>,
+	    xmpp:make_error(IQ, xmpp:err_not_allowed(Txt, Lang))
+    end;
+process_iq(#iq{type = get, lang = Lang} = IQ) ->
+    Txt = <<"Value 'get' of 'type' attribute is not allowed">>,
+    xmpp:make_error(IQ, xmpp:err_not_allowed(Txt, Lang));
+process_iq(#iq{lang = Lang} = IQ) ->
+    ?INFO_MSG("IQ = ~p", [IQ]),
+    Txt = <<"No module is handling this query">>,
+    xmpp:make_error(IQ, xmpp:err_service_unavailable(Txt, Lang)).
+
 process(_Handlers,
 	#request{method = 'GET', lang = Lang,
 		 path = [_, Id]}) ->
@@ -261,12 +282,30 @@ process(_Handlers,
 process(_Handlers, _Request) ->
     ejabberd_web:error(not_found).
 
+host_up(Host) ->
+    IQDisc = gen_iq_handler:iqdisc(Host),
+    gen_iq_handler:add_iq_handler(ejabberd_sm, Host, ?NS_CAPTCHA,
+				  ?MODULE, process_iq, IQDisc).
+
+host_down(Host) ->
+    gen_iq_handler:remove_iq_handler(ejabberd_sm, Host, ?NS_CAPTCHA).
+
+config_reloaded() ->
+    gen_server:call(?MODULE, config_reloaded, timer:minutes(1)).
+
 init([]) ->
     mnesia:delete_table(captcha),
-    ets:new(captcha,
-	    [named_table, public, {keypos, #captcha.id}]),
-    check_captcha_setup(),
-    {ok, #state{}}.
+    ets:new(captcha, [named_table, public, {keypos, #captcha.id}]),
+    case check_captcha_setup() of
+	true ->
+	    register_handlers(),
+	    ejabberd_hooks:add(config_reloaded, ?MODULE, config_reloaded, 50),
+	    {ok, #state{enabled = true}};
+	false ->
+	    {ok, #state{enabled = false}};
+	{error, Reason} ->
+	    {stop, Reason}
+    end.
 
 handle_call({is_limited, Limiter, RateLimit}, _From,
 	    State) ->
@@ -285,6 +324,23 @@ handle_call({is_limited, Limiter, RateLimit}, _From,
 				   Limits),
 	  {reply, false, State#state{limits = NewLimits}}
     end;
+handle_call(config_reloaded, _From, #state{enabled = Enabled} = State) ->
+    State1 = case is_feature_available() of
+		 true when not Enabled ->
+		     case check_captcha_setup() of
+			 true ->
+			     register_handlers(),
+			     State#state{enabled = true};
+			 _ ->
+			     State
+		     end;
+		 false when Enabled ->
+		     unregister_handlers(),
+		     State#state{enabled = false};
+		 _ ->
+		     State
+	     end,
+    {reply, ok, State1};
 handle_call(_Request, _From, State) ->
     {reply, bad_request, State}.
 
@@ -293,17 +349,29 @@ handle_cast(_Msg, State) -> {noreply, State}.
 handle_info({remove_id, Id}, State) ->
     ?DEBUG("captcha ~p timed out", [Id]),
     case ets:lookup(captcha, Id) of
-      [#captcha{args = Args, pid = Pid}] ->
-	  if is_pid(Pid) -> Pid ! {captcha_failed, Args};
-	     true -> ok
-	  end,
-	  ets:delete(captcha, Id);
-      _ -> ok
+	[#captcha{args = Args, pid = Pid}] ->
+	    callback(captcha_failed, Pid, Args),
+	    ets:delete(captcha, Id);
+	_ -> ok
     end,
     {noreply, State};
 handle_info(_Info, State) -> {noreply, State}.
 
-terminate(_Reason, _State) -> ok.
+terminate(_Reason, #state{enabled = Enabled}) ->
+    if Enabled -> unregister_handlers();
+       true -> ok
+    end,
+    ejabberd_hooks:delete(config_reloaded, ?MODULE, config_reloaded, 50).
+
+register_handlers() ->
+    ejabberd_hooks:add(host_up, ?MODULE, host_up, 50),
+    ejabberd_hooks:add(host_down, ?MODULE, host_down, 50),
+    lists:foreach(fun host_up/1, ?MYHOSTS).
+
+unregister_handlers() ->
+    ejabberd_hooks:delete(host_up, ?MODULE, host_up, 50),
+    ejabberd_hooks:delete(host_down, ?MODULE, host_down, 50),
+    lists:foreach(fun host_down/1, ?MYHOSTS).
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
@@ -467,16 +535,18 @@ is_feature_available() ->
 
 check_captcha_setup() ->
     case is_feature_available() of
-      true ->
-	  case create_image() of
-	    {ok, _, _, _} -> ok;
-	    _Err ->
-		?CRITICAL_MSG("Captcha is enabled in the option captcha_cmd, "
-			      "but it can't generate images.",
-			      []),
-		throw({error, captcha_cmd_enabled_but_fails})
-	  end;
-      false -> ok
+	true ->
+	    case create_image() of
+		{ok, _, _, _} ->
+		    true;
+		Err ->
+		    ?CRITICAL_MSG("Captcha is enabled in the option captcha_cmd, "
+				  "but it can't generate images.",
+				  []),
+		    Err
+	    end;
+	false ->
+	    false
     end.
 
 lookup_captcha(Id) ->
@@ -491,22 +561,18 @@ lookup_captcha(Id) ->
 
 check_captcha(Id, ProvidedKey) ->
     case ets:lookup(captcha, Id) of
-      [#captcha{pid = Pid, args = Args, key = ValidKey,
-		tref = Tref}] ->
-	  ets:delete(captcha, Id),
-	  erlang:cancel_timer(Tref),
-	  if ValidKey == ProvidedKey ->
-		 if is_pid(Pid) -> Pid ! {captcha_succeed, Args};
-		    true -> ok
-		 end,
-		 captcha_valid;
-	     true ->
-		 if is_pid(Pid) -> Pid ! {captcha_failed, Args};
-		    true -> ok
-		 end,
-		 captcha_non_valid
-	  end;
-      _ -> captcha_not_found
+	[#captcha{pid = Pid, args = Args, key = ValidKey, tref = Tref}] ->
+	    ets:delete(captcha, Id),
+	    erlang:cancel_timer(Tref),
+	    if ValidKey == ProvidedKey ->
+		    callback(captcha_succeed, Pid, Args),
+		    captcha_valid;
+	       true ->
+		    callback(captcha_failed, Pid, Args),
+		    captcha_non_valid
+	    end;
+	_ ->
+	    captcha_not_found
     end.
 
 clean_treap(Treap, CleanPriority) ->
@@ -519,6 +585,14 @@ clean_treap(Treap, CleanPriority) ->
 	     true -> Treap
 	  end
     end.
+
+-spec callback(captcha_succeed | captcha_failed, pid(), term()) -> any().
+callback(Result, _Pid, F) when is_function(F) ->
+    F(Result);
+callback(Result, Pid, Args) when is_pid(Pid) ->
+    Pid ! {Result, Args};
+callback(_, _, _) ->
+    ok.
 
 now_priority() ->
     -p1_time_compat:system_time(micro_seconds).

src/ejabberd_sm.erl
@@ -43,7 +43,7 @@
 	 open_session/5,
 	 open_session/6,
 	 close_session/4,
-	 check_in_subscription/6,
+	 check_in_subscription/2,
 	 bounce_offline_message/1,
 	 disconnect_removed_user/2,
 	 get_user_resources/2,
@@ -183,10 +183,9 @@ close_session(SID, User, Server, Resource) ->
     ejabberd_hooks:run(sm_remove_connection_hook,
 		       JID#jid.lserver, [SID, JID, Info]).
 
--spec check_in_subscription(boolean(), binary(), binary(), jid(),
-			    subscribe | subscribed | unsubscribe | unsubscribed,
-			    binary()) -> boolean() | {stop, false}.
-check_in_subscription(Acc, User, Server, _JID, _Type, _Reason) ->
+-spec check_in_subscription(boolean(), presence()) -> boolean() | {stop, false}.
+check_in_subscription(Acc, #presence{to = To}) ->
+    #jid{user = User, server = Server} = To,
     case ejabberd_auth:user_exists(User, Server) of
       true -> Acc;
       false -> {stop, false}
@@ -627,19 +626,14 @@ do_route(To, Term) ->
     end.
 
 -spec do_route(stanza()) -> any().
-do_route(#presence{from = From, to = To, type = T, status = Status} = Packet)
+do_route(#presence{to = To, type = T} = Packet)
   when T == subscribe; T == subscribed; T == unsubscribe; T == unsubscribed ->
     ?DEBUG("processing subscription:~n~s", [xmpp:pp(Packet)]),
-    #jid{user = User, server = Server,
-	 luser = LUser, lserver = LServer} = To,
-    Reason = if T == subscribe -> xmpp:get_text(Status);
-		true -> <<"">>
-	     end,
+    #jid{luser = LUser, lserver = LServer} = To,
     case is_privacy_allow(Packet) andalso
 	ejabberd_hooks:run_fold(
 	  roster_in_subscription,
-	  LServer, false,
-	  [User, Server, From, T, Reason]) of
+	  LServer, false, [Packet]) of
 	true ->
 	    Mod = get_sm_backend(LServer),
 	    lists:foreach(

src/ejabberd_sup.erl
@@ -166,7 +166,6 @@ init([]) ->
 	   ACME,
 	   Listener,
 	   S2S,
-	   Captcha,
 	   S2SInSupervisor,
 	   S2SOutSupervisor,
 	   ServiceSupervisor,
@@ -182,6 +181,7 @@ init([]) ->
 	   RouterMulticast,
 	   Local,
 	   SM,
+	   Captcha,
 	   ExtMod,
 	   GenModSupervisor,
 	   Auth,

src/mod_block_strangers.erl
@@ -32,7 +32,7 @@
 -export([start/2, stop/1, reload/3,
          depends/2, mod_opt_type/1, mod_options/1]).
 
--export([filter_packet/1, filter_offline_msg/1]).
+-export([filter_packet/1, filter_offline_msg/1, filter_subscription/2]).
 
 -include("xmpp.hrl").
 -include("ejabberd.hrl").
@@ -40,15 +40,22 @@
 
 -define(SETS, gb_sets).
 
+%%%===================================================================
+%%% Callbacks and hooks
+%%%===================================================================
 start(Host, _Opts) ->
     ejabberd_hooks:add(user_receive_packet, Host,
                        ?MODULE, filter_packet, 25),
+    ejabberd_hooks:add(roster_in_subscription, Host,
+		       ?MODULE, filter_subscription, 25),
     ejabberd_hooks:add(offline_message_hook, Host,
 		       ?MODULE, filter_offline_msg, 25).
 
 stop(Host) ->
     ejabberd_hooks:delete(user_receive_packet, Host,
                           ?MODULE, filter_packet, 25),
+    ejabberd_hooks:delete(roster_in_subscription, Host,
+			  ?MODULE, filter_subscription, 25),
     ejabberd_hooks:delete(offline_message_hook, Host,
 			  ?MODULE, filter_offline_msg, 25).
 
@@ -79,17 +86,72 @@ filter_offline_msg({_Action, #message{} = Msg} = Acc) ->
 	deny -> {stop, {drop, Msg}}
     end.
 
+filter_subscription(Acc, #presence{meta = #{captcha := passed}}) ->
+    Acc;
+filter_subscription(Acc, #presence{from = From, to = To, lang = Lang,
+				   id = SID, type = subscribe} = Pres) ->
+    LServer = To#jid.lserver,
+    case gen_mod:get_module_opt(LServer, ?MODULE, drop) andalso
+	 gen_mod:get_module_opt(LServer, ?MODULE, captcha) andalso
+	 need_check(Pres) of
+	true ->
+	    case check_subscription(From, To) of
+		false ->
+		    BFrom = jid:remove_resource(From),
+		    BTo = jid:remove_resource(To),
+		    Limiter = jid:tolower(BFrom),
+		    case ejabberd_captcha:create_captcha(
+			   SID, BFrom, BTo, Lang, Limiter,
+			   fun(Res) -> handle_captcha_result(Res, Pres) end) of
+			{ok, ID, Body, CaptchaEls} ->
+			    Msg = #message{from = BTo, to = From,
+					   id = ID, body = Body,
+					   sub_els = CaptchaEls},
+			    case gen_mod:get_module_opt(LServer, ?MODULE, log) of
+				true ->
+				    ?INFO_MSG("Challenge subscription request "
+					      "from stranger ~s to ~s with "
+					      "CAPTCHA",
+					      [jid:encode(From), jid:encode(To)]);
+				false ->
+				    ok
+			    end,
+			    ejabberd_router:route(Msg);
+			{error, limit} ->
+			    ErrText = <<"Too many CAPTCHA requests">>,
+			    Err = xmpp:err_resource_constraint(ErrText, Lang),
+			    ejabberd_router:route_error(Pres, Err);
+			_ ->
+			    ErrText = <<"Unable to generate a CAPTCHA">>,
+			    Err = xmpp:err_internal_server_error(ErrText, Lang),
+			    ejabberd_router:route_error(Pres, Err)
+		    end,
+		    {stop, false};
+		true ->
+		    Acc
+	    end;
+	false ->
+	    Acc
+    end;
+filter_subscription(Acc, _) ->
+    Acc.
+
+handle_captcha_result(captcha_succeed, Pres) ->
+    Pres1 = xmpp:put_meta(Pres, captcha, passed),
+    ejabberd_router:route(Pres1);
+handle_captcha_result(captcha_failed, #presence{lang = Lang} = Pres) ->
+    Txt = <<"The CAPTCHA verification has failed">>,
+    ejabberd_router:route_error(Pres, xmpp:err_not_allowed(Txt, Lang)).
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 check_message(#message{from = From, to = To, lang = Lang} = Msg) ->
     LServer = To#jid.lserver,
-    AllowLocalUsers =
-        gen_mod:get_module_opt(LServer, ?MODULE, allow_local_users),
-    case (Msg#message.body == [] andalso
-          Msg#message.subject == [])
-        orelse ((AllowLocalUsers orelse From#jid.luser == <<"">>) andalso
-                ejabberd_router:is_my_host(From#jid.lserver)) of
-	false ->
+    case need_check(Msg) of
+	true ->
 	    case check_subscription(From, To) of
-		none ->
+		false ->
 		    Drop = gen_mod:get_module_opt(LServer, ?MODULE, drop),
 		    Log = gen_mod:get_module_opt(LServer, ?MODULE, log),
 		    if
@@ -112,10 +174,10 @@ check_message(#message{from = From, to = To, lang = Lang} = Msg) ->
 			true ->
 			    allow
 		    end;
-		some ->
+		true ->
 		    allow
 	    end;
-	true ->
+	false ->
 	    allow
     end.
 
@@ -125,33 +187,45 @@ maybe_adjust_from(#message{type = groupchat, from = From} = Msg) ->
 maybe_adjust_from(#message{} = Msg) ->
     Msg.
 
--spec check_subscription(jid(), jid()) -> none | some.
+-spec need_check(presence() | message()) -> boolean().
+need_check(Pkt) ->
+    To = xmpp:get_to(Pkt),
+    From = xmpp:get_from(Pkt),
+    LServer = To#jid.lserver,
+    IsEmpty = case Pkt of
+		  #message{body = [], subject = []} ->
+		      true;
+		  _ ->
+		      false
+	      end,
+    AllowLocalUsers = gen_mod:get_module_opt(LServer, ?MODULE, allow_local_users),
+    not (IsEmpty orelse ((AllowLocalUsers orelse From#jid.luser == <<"">>)
+			 andalso ejabberd_router:is_my_host(From#jid.lserver))).
+
+-spec check_subscription(jid(), jid()) -> boolean().
 check_subscription(From, To) ->
     {LocalUser, LocalServer, _} = jid:tolower(To),
     {RemoteUser, RemoteServer, _} = jid:tolower(From),
-    case ejabberd_hooks:run_fold(
-	   roster_get_jid_info, LocalServer,
-	   {none, []}, [LocalUser, LocalServer, From]) of
-	{none, _} when RemoteUser == <<"">> ->
-	    none;
-	{none, _} ->
-	    case gen_mod:get_module_opt(LocalServer, ?MODULE,
-					allow_transports) of
-		true ->
-		    %% Check if the contact's server is in the roster
-		    case ejabberd_hooks:run_fold(
-			   roster_get_jid_info, LocalServer,
-			   {none, []},
-			   [LocalUser, LocalServer, jid:make(RemoteServer)]) of
-			{none, _} -> none;
-			_ -> some
-		    end;
-		false ->
-		    none
-	    end;
-	_ ->
-	    some
+    case has_subscription(LocalUser, LocalServer, From) of
+	false when RemoteUser == <<"">> ->
+	    false;
+	false ->
+	    %% Check if the contact's server is in the roster
+	    gen_mod:get_module_opt(LocalServer, ?MODULE, allow_transports)
+		andalso has_subscription(LocalUser, LocalServer,
+					 jid:make(RemoteServer));
+	true ->
+	    true
     end.
+
+-spec has_subscription(binary(), binary(), jid()) -> boolean().
+has_subscription(User, Server, JID) ->
+    {Sub, Ask, _} = ejabberd_hooks:run_fold(
+		      roster_get_jid_info, Server,
+		      {none, none, []},
+		      [User, Server, JID]),
+    (Sub /= none) orelse (Ask == subscribe)
+	orelse (Ask == out) orelse (Ask == both).
 
 sets_bare_member({U, S, <<"">>} = LBJID, Set) ->
     case ?SETS:next(sets_iterator_from(LBJID, Set)) of
@@ -189,10 +263,13 @@ mod_opt_type(log) ->
 mod_opt_type(allow_local_users) ->
     fun (B) when is_boolean(B) -> B end;
 mod_opt_type(allow_transports) ->
+    fun (B) when is_boolean(B) -> B end;
+mod_opt_type(captcha) ->
     fun (B) when is_boolean(B) -> B end.
 
 mod_options(_) ->
     [{drop, true},
      {log, false},
+     {captcha, false},
      {allow_local_users, true},
      {allow_transports, true}].

src/mod_caps.erl
@@ -203,9 +203,10 @@ disco_info(Acc, _, _, _Node, _Lang) ->
 -spec c2s_presence_in(ejabberd_c2s:state(), presence()) -> ejabberd_c2s:state().
 c2s_presence_in(C2SState,
 		#presence{from = From, to = To, type = Type} = Presence) ->
-    {Subscription, _} = ejabberd_hooks:run_fold(
-			  roster_get_jid_info, To#jid.lserver,
-			  {none, []}, [To#jid.luser, To#jid.lserver, From]),
+    {Subscription, _, _} = ejabberd_hooks:run_fold(
+			     roster_get_jid_info, To#jid.lserver,
+			     {none, none, []},
+			     [To#jid.luser, To#jid.lserver, From]),
     ToSelf = (From#jid.luser == To#jid.luser)
 	       and (From#jid.lserver == To#jid.lserver),
     Insert = (Type == available)

src/mod_mam.erl
@@ -672,10 +672,10 @@ should_archive_peer(LUser, LServer,
 			always -> true;
 			never -> false;
 			roster ->
-			    {Sub, _} = ejabberd_hooks:run_fold(
-					 roster_get_jid_info,
-					 LServer, {none, []},
-					 [LUser, LServer, Peer]),
+			    {Sub, _, _} = ejabberd_hooks:run_fold(
+					    roster_get_jid_info,
+					    LServer, {none, none, []},
+					    [LUser, LServer, Peer]),
 			    Sub == both orelse Sub == from orelse Sub == to
 		    end
 	    end

commit d5afc767e662b12d98e4c57c8d037f1c75c03a4f

Fix 'badmatch' crash
The crash was introduced in 4b012a99d2bdd6d22f05676e9a7989409e314fca

src/mod_last.erl
@@ -156,9 +156,9 @@ privacy_check_packet(allow, C2SState,
     case xmpp:has_subtag(IQ, #last{}) of
 	true ->
 	    #jid{luser = LUser, lserver = LServer} = To,
-	    {Sub, _} = ejabberd_hooks:run_fold(
-			 roster_get_jid_info, LServer,
-			 {none, []}, [LUser, LServer, From]),
+	    {Sub, _, _} = ejabberd_hooks:run_fold(
+			    roster_get_jid_info, LServer,
+			    {none, none, []}, [LUser, LServer, From]),
 	    if Sub == from; Sub == both ->
 		    Pres = #presence{from = To, to = From},
 		    case ejabberd_hooks:run_fold(