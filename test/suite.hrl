82c42051c3b8f85e187c0ead4e66dfdfac2595a2
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2018-05-17 13:24:23 +0300

    Correctly resolve upload.localhost for the test suite

--- test/suite.hrl
+++ test/suite.hrl
@@ -84,6 +84,7 @@
 -define(EXTAUTH_VHOST, <<"extauth.localhost">>).
 -define(RIAK_VHOST, <<"riak.localhost">>).
 -define(S2S_VHOST, <<"s2s.localhost">>).
+-define(UPLOAD_VHOST, <<"upload.localhost">>).
 
 insert(Val, N, Tuple) ->
     L = tuple_to_list(Tuple),
