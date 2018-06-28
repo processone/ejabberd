8f2233eff74363f8434ab8769e85f4154e8555fd
Paweł Chmielowski <pchmielowski@process-one.net>
2018-06-28 10:56:59 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Ejabberd.Mixfile do
     [{:lager, "~> 3.4.0"},
      {:p1_utils, "~> 1.0"},
      {:fast_xml, "~> 1.1"},
-     {:xmpp, github: "processone/xmpp", tag: "e26a39a"},
+     {:xmpp, "~> 1.2"},
      {:cache_tab, "~> 1.0"},
      {:stringprep, "~> 1.0"},
      {:fast_yaml, "~> 1.0"},
