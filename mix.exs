989da356c47f42a99339f4c21f9707850831e465
Paweł Chmielowski <pchmielowski@process-one.net>
2018-10-09 11:33:39 +0200

    Add pkix to included_applications

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Ejabberd.Mixfile do
      included_applications: [:lager, :mnesia, :inets, :p1_utils, :cache_tab,
                              :fast_tls, :stringprep, :fast_xml, :xmpp,
                              :stun, :fast_yaml, :esip, :jiffy, :p1_oauth2,
-                             :eimp, :base64url, :jose]
+                             :eimp, :base64url, :jose, :pkix]
                          ++ cond_apps()]
   end
 
