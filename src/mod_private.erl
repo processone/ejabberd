c3a807832dac01d0668233853c17deb73702114f
Holger Weiss <holger@zedat.fu-berlin.de>
2019-01-08 21:23:21 +0100

    mod_private: Hardcode item ID for PEP bookmarks
    Set the item ID to "current" when publishing bookmarks via PEP.
    XEP-0060 says: For singleton nodes, "it is RECOMMENDED for the publisher
    to specify an ItemID of 'current' to ensure that the publication of a
    new item will overwrite the existing item."

--- src/mod_private.erl
+++ src/mod_private.erl
@@ -255,7 +255,7 @@ publish_data(JID, Data) ->
 			       {access_model, whitelist}],
 		    case mod_pubsub:publish_item(
 			   LBJID, LServer, ?NS_STORAGE_BOOKMARKS, JID,
-			   <<>>, [El], PubOpts, all) of
+			   <<"current">>, [El], PubOpts, all) of
 			{result, _} -> ok;
 			{error, _} = Err -> Err
 		    end
