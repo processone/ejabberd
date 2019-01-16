3f2265d4575b76d6199a807470024ca9e97d3f19
Paweł Chmielowski <pchmielowski@process-one.net>
2019-01-16 09:58:24 +0100

    Update xmpp in mix

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Ejabberd.Mixfile do
     [{:lager, "~> 3.6.0"},
      {:p1_utils, "~> 1.0"},
      {:fast_xml, "~> 1.1"},
-     {:xmpp, "~> 1.2"},
+     {:xmpp, "~> 1.3.0"},
      {:cache_tab, "~> 1.0"},
      {:stringprep, "~> 1.0"},
      {:fast_yaml, "~> 1.0"},
