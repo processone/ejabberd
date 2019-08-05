c2d97854e36498d91f8c1a70ffa01e706444e5e2
Paweł Chmielowski <pchmielowski@process-one.net>
2019-08-05 10:15:06 +0200

    Bump version in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ejabberd.Mixfile do
 
   def project do
     [app: :ejabberd,
-     version: "19.5.0",
+     version: "19.8.0",
      description: description(),
      elixir: "~> 1.4",
      elixirc_paths: ["lib"],
