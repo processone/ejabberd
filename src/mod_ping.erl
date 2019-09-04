processone/ejabberd

commit dd57950103f960da32fc8988835af5209f47c78a

Fix ping IQ reply/timeout processing ("mod_ping" regression since 17.x that may cause resources leakage)

src/mod_ping.erl
@@ -122,30 +122,31 @@ handle_cast({start_ping, JID}, State) ->
 handle_cast({stop_ping, JID}, State) ->
     Timers = del_timer(JID, State#state.timers),
     {noreply, State#state{timers = Timers}};
-handle_cast({iq_reply, timeout, JID}, State) ->
-    ejabberd_hooks:run(user_ping_timeout, State#state.host,
-		       [JID]),
-    Timers = case State#state.timeout_action of
-		 kill ->
-		     #jid{user = User, server = Server,
-			  resource = Resource} =
-			 JID,
-		     case ejabberd_sm:get_session_pid(User, Server, Resource)
-		     of
-			 Pid when is_pid(Pid) -> ejabberd_c2s:close(Pid, ping_timeout);
-			 _ -> ok
-		     end,
-		     del_timer(JID, State#state.timers);
-		 _ ->
-		     State#state.timers
-	     end,
-    {noreply, State#state{timers = Timers}};
-handle_cast({iq_reply, #iq{}, _JID}, State) ->
-    {noreply, State};
 handle_cast(Msg, State) ->
     ?WARNING_MSG("unexpected cast: ~p", [Msg]),
     {noreply, State}.
 
+handle_info({iq_reply, #iq{type = error}, JID}, State) ->
+    handle_info({iq_reply, timeout, JID}, State);
+handle_info({iq_reply, #iq{}, _JID}, State) ->
+    {noreply, State};
+handle_info({iq_reply, timeout, JID}, State) ->
+    Timers = del_timer(JID, State#state.timers),
+    ejabberd_hooks:run(user_ping_timeout, State#state.host,
+		       [JID]),
+    case State#state.timeout_action of
+      kill ->
+	  #jid{user = User, server = Server,
+	       resource = Resource} =
+	      JID,
+	  case ejabberd_sm:get_session_pid(User, Server, Resource)
+	      of
+	    Pid when is_pid(Pid) -> ejabberd_c2s:close(Pid, ping_timeout);
+	    _ -> ok
+	  end;
+      _ -> ok
+    end,
+    {noreply, State#state{timers = Timers}};
 handle_info({timeout, _TRef, {ping, JID}}, State) ->
     Host = State#state.host,
     From = jid:remove_resource(JID),

commit 9a8245b8e6a17b306d07cd96f2483044dd4981d2

Fix the regression introduced in dd57950
Fixes #3010

src/mod_ping.erl
@@ -132,8 +132,6 @@ handle_cast(Msg, State) ->
     ?WARNING_MSG("Unexpected cast: ~p", [Msg]),
     {noreply, State}.
 
-handle_info({iq_reply, #iq{type = error}, JID}, State) ->
-    handle_info({iq_reply, timeout, JID}, State);
 handle_info({iq_reply, #iq{}, _JID}, State) ->
     {noreply, State};
 handle_info({iq_reply, timeout, JID}, State) ->