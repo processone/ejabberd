72f6835feb8566c6a996c904824654f3f8eee436
Evgeny Khramtsov <ekhramtsov@process-one.net>
2019-10-29 13:36:40 +0300

    Fix race condition in Redis/SQL supervisors startup

--- src/ejabberd_redis_sup.erl
+++ src/ejabberd_redis_sup.erl
@@ -45,7 +45,10 @@ start() ->
 		    permanent, infinity, supervisor, [?MODULE]},
 	    case supervisor:start_child(ejabberd_db_sup, Spec) of
 		{ok, _} -> ok;
-		{error, {already_started, _}} -> ok;
+		{error, {already_started, Pid}} ->
+                    %% Wait for the supervisor to fully start
+                    _ = supervisor:count_children(Pid),
+                    ok;
 		{error, Why} = Err ->
 		    ?ERROR_MSG("Failed to start ~ts: ~p", [?MODULE, Why]),
 		    Err
