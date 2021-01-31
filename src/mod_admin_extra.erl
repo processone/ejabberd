processone/ejabberd

commit 7fc500dae6c7c2c8db95e56607f179aa9f00dd9c

Don't include empty <subject/> in messages sent by send_message
Please refer issue #3485

src/mod_admin_extra.erl
@@ -1494,10 +1494,15 @@ send_message(Type, From, To, Subject, Body) ->
                       #xmlel{name = <<"body">>,
                              children = [{xmlcdata, Body}]}]},
           ?NS_CLIENT, CodecOpts) of
-        #message{from = JID} = Msg ->
+        #message{from = JID, subject = Subject, body = Body} = Msg ->
+            Msg2 = case {xmpp:get_text(Subject), xmpp:get_text(Body)} of
+                       {_, <<>>} -> Msg;
+                       {<<>>, _} -> Msg#message{subject = []};
+                       _ -> Msg
+                   end,
             State = #{jid => JID},
-            ejabberd_hooks:run_fold(user_send_packet, JID#jid.lserver, {Msg, State}, []),
-            ejabberd_router:route(Msg)
+            ejabberd_hooks:run_fold(user_send_packet, JID#jid.lserver, {Msg2, State}, []),
+            ejabberd_router:route(Msg2)
     catch _:{xmpp_codec, Why} ->
             {error, xmpp:format_error(Why)}
     end.

commit b3d9c0d1f7f7037c1a22161c857c931f5e4b7ed3

Fix bug in send_message introduced in 7fc500dae (#3485)

src/mod_admin_extra.erl
@@ -1494,10 +1494,10 @@ send_message(Type, From, To, Subject, Body) ->
                       #xmlel{name = <<"body">>,
                              children = [{xmlcdata, Body}]}]},
           ?NS_CLIENT, CodecOpts) of
-        #message{from = JID, subject = Subject, body = Body} = Msg ->
-            Msg2 = case {xmpp:get_text(Subject), xmpp:get_text(Body)} of
-                       {_, <<>>} -> Msg;
-                       {<<>>, _} -> Msg#message{subject = []};
+        #message{from = JID, subject = SubjectEl, body = BodyEl} = Msg ->
+            Msg2 = case {xmpp:get_text(SubjectEl), xmpp:get_text(BodyEl)} of
+                       {Subject, <<>>} -> Msg;
+                       {<<>>, Body} -> Msg#message{subject = []};
                        _ -> Msg
                    end,
             State = #{jid => JID},