5bc900d53d31318ab725c658fbb3741214d13fea
Adrien Bourmault <neox@os-k.eu>
2021-02-10 14:58:37 +0100

    New captcha.sh question/answer based

--- tools/captcha.sh
+++ tools/captcha.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+# Copyright © 2021 Adrien Bourmault (neox@os-k.eu)
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
 # This script is an example captcha script.
 # It takes the text to recognize in the captcha image as a parameter.
@@ -15,13 +21,16 @@
 
 INPUT=$1
 
+TRANSFORMATIONS=(INTRUDER SUM)
+DIGIT=(zero one two three four five six seven eight nine ten)
+
 if test -n ${BASH_VERSION:-''} ; then
     get_random ()
     {  
 	R=$RANDOM
     }
 else
-    for n in `od -A n -t u2 -N 48 /dev/urandom`; do RL="$RL$n "; done
+    for n in `od -A n -t u2 -N 64 /dev/urandom`; do RL="$RL$n "; done
     get_random ()
     {  
 	R=${RL%% *}
@@ -29,72 +38,95 @@ else
     }
 fi
 
-get_random
-WAVE1_AMPLITUDE=$((2 + $R % 5))
-get_random
-WAVE1_LENGTH=$((50 + $R % 25))
-get_random
-WAVE2_AMPLITUDE=$((2 + $R % 5))
-get_random
-WAVE2_LENGTH=$((50 + $R % 25))
-get_random
-WAVE3_AMPLITUDE=$((2 + $R % 5))
-get_random
-WAVE3_LENGTH=$((50 + $R % 25))
-get_random
-W1_LINE_START_Y=$((10 + $R % 40))
-get_random
-W1_LINE_STOP_Y=$((10 + $R % 40))
-get_random
-W2_LINE_START_Y=$((10 + $R % 40))
-get_random
-W2_LINE_STOP_Y=$((10 + $R % 40))
-get_random
-W3_LINE_START_Y=$((10 + $R % 40))
-get_random
-W3_LINE_STOP_Y=$((10 + $R % 40))
+
+INTRUDER()
+{
+NUMBERS=$(echo $INPUT | grep -o . | tr '\n' ' ')
+SORTED_UNIQ_NUM=$(echo "${NUMBERS[@]}" | sort -u | tr '\n' ' ')
+RANDOM_DIGITS=$(echo 123456789 | grep -o . | sort -R | tr '\n' ' ')
+INTRUDER=-1
+
+for i in ${RANDOM_DIGITS[@]}
+do
+    if [[ ! " ${SORTED_UNIQ_NUM[@]} " =~ " ${i} " ]]; then
+        INTRUDER=$i
+        break
+    fi
+done
+
+# Worst case
+if [[ $INTRUDER -eq "-1" ]]
+then
+    echo Type "$INPUT \n   without changes"
+    return
+fi
+
+for num in ${NUMBERS[@]}
+do
+    get_random
+    R=$(($R % 100))
+
+    if [[ $R -lt 60 ]]; then
+        NEWINPUT=${NEWINPUT}${num}${INTRUDER}
+    else
+        NEWINPUT=${NEWINPUT}${num}
+    fi
+done
 
 get_random
-B1_LINE_START_Y=$(($R % 40))
+R=$(($R % 100))
+
+if [[ $R -lt 50 ]]; then
+    echo "Type $NEWINPUT by\n   deleting the '${DIGIT[$INTRUDER]}'"
+else
+    echo "Enter $NEWINPUT by\n   removing the '${DIGIT[$INTRUDER]}'"
+fi
+}
+
+
+SUM()
+{
 get_random
-B1_LINE_STOP_Y=$(($R % 40))
-get_random
-B2_LINE_START_Y=$(($R % 40))
-get_random
-B2_LINE_STOP_Y=$(($R % 40))
-#B3_LINE_START_Y=$(($R % 40))
-#B3_LINE_STOP_Y=$(($R % 40))
+RA=$(($R % 100))
+
+if [[ $(($INPUT % 2)) -eq 0 ]]; then
+    A=$(($INPUT - $RA))
+    B=$RA
+else
+    B=$(($INPUT - $RA))
+    A=$RA
+fi
 
 get_random
-B1_LINE_START_X=$(($R % 20))
-get_random
-B1_LINE_STOP_X=$((100 + $R % 40))
-get_random
-B2_LINE_START_X=$(($R % 20))
-get_random
-B2_LINE_STOP_X=$((100 + $R % 40))
-#B3_LINE_START_X=$(($R % 20))
-#B3_LINE_STOP_X=$((100 + $R % 40))
+R=$(($R % 100))
+
+if [[ $R -lt 25 ]]; then
+    echo "Type the result\n   of $A + $B"
+elif [[ $R -lt 50 ]]; then
+    echo "SUMz\n   $A and $B"
+elif [[ $R -lt 75 ]]; then
+    echo "Add\n   $A and $B"
+else
+    echo "Enter the result\n   of $A + $B"
+fi
+}
+
 
 get_random
-ROLL_X=$(($R % 40))
 
-convert -size 180x60 xc:none -pointsize 40 \
-	\( -clone 0 -fill white \
-	-stroke black -strokewidth 4 -annotate +0+40 "$INPUT" \
-	-stroke white -strokewidth 2 -annotate +0+40 "$INPUT" \
-	-roll +$ROLL_X+0 \
-	-wave "$WAVE1_AMPLITUDE"x"$WAVE1_LENGTH" \
-	-roll -$ROLL_X+0 \) \
-	\( -clone 0 -stroke black \
-	-strokewidth 1 -draw \
-	"line $B1_LINE_START_X,$B1_LINE_START_Y $B1_LINE_STOP_X,$B1_LINE_STOP_Y" \
-	-strokewidth 1 -draw \
-	"line $B2_LINE_START_X,$B2_LINE_START_Y $B2_LINE_STOP_X,$B2_LINE_STOP_Y" \
-	-wave "$WAVE2_AMPLITUDE"x"$WAVE2_LENGTH" \) \
-	\( -clone 0 -stroke white \
-	-strokewidth 2 -draw "line 0,$W1_LINE_START_Y 140,$W1_LINE_STOP_Y" \
-	-strokewidth 2 -draw "line 0,$W2_LINE_START_Y 140,$W2_LINE_STOP_Y" \
-	-strokewidth 2 -draw "line 0,$W3_LINE_START_Y 140,$W3_LINE_STOP_Y" \
-	-wave "$WAVE3_AMPLITUDE"x"$WAVE3_LENGTH" \) \
-	-flatten -crop 140x60 +repage -quality 90 -depth 8 png:-
+RAND_ITALIC=$(($R % 25))
+get_random
+
+RAND_ANGLE=$(($R % 3))
+get_random
+
+RAND_INDEX=$(($R % ${#TRANSFORMATIONS[@]}))
+
+convert -size 300x60 xc:none -pointsize 20 \
+\( -clone 0 -fill black \
+-stroke black -strokewidth 1 \
+-annotate "${RAND_ANGLE}x${RAND_ITALIC}+0+0" "\n $(${TRANSFORMATIONS[$RAND_INDEX]})" \
+-roll +$ROLL_X+0 \
+-wave "$WAVE1_AMPLITUDE"x"$WAVE1_LENGTH" \
+-roll -$ROLL_X+0 \) \
+-flatten -crop 300x60 +repage -quality 500 -depth 11 png:-
