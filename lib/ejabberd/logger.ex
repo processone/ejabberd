8b78d27f300fd10b2648af591b0486d6b1dc5485
Badlop <badlop@process-one.net>
2021-03-10 17:58:27 +0100

    Update Elixir logger, so logging should work now

--- lib/ejabberd/logger.ex
+++ lib/ejabberd/logger.ex
@@ -1,9 +1,9 @@
 defmodule Ejabberd.Logger do
 
-  def critical(message, args \\ []), do: :lager.log(:critical, [], message, args)
-  def error(message, args \\ []),    do: :lager.log(:error, [], message, args)
-  def warning(message, args \\ []),  do: :lager.log(:warning, [], message, args)
-  def info(message, args \\ []),     do: :lager.log(:info, [], message, args)
-  def debug(message, args \\ []),    do: :lager.log(:debug, [], message, args)
+  def critical(message, args \\ []), do: :logger.critical(message, args)
+  def error(message, args \\ []),    do: :logger.error(message, args)
+  def warning(message, args \\ []),  do: :logger.warning(message, args)
+  def info(message, args \\ []),     do: :logger.info(message, args)
+  def debug(message, args \\ []),    do: :logger.debug( message, args)
 
 end
