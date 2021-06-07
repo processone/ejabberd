9a93562b94512b1d33ff8eeaf51925fdb1569e5b
Badlop <badlop@process-one.net>
2021-06-07 16:55:47 +0200

    Revert luerl from v0.4 which has a compilation warning; v0.3 is enough
    Revert "Update luerl dependency from v0.3 to v0.4"
    
    This reverts commit 64f3f3287a3ae40ffa471558835a1a4155f5e0e4.

--- mix.exs
+++ mix.exs
@@ -130,7 +130,7 @@ defmodule Ejabberd.Mixfile do
     for {:true, dep} <- [{config(:pam), {:epam, "~> 1.0"}},
                          {config(:redis), {:eredis, "~> 1.2.0"}},
                          {config(:zlib), {:ezlib, "~> 1.0"}},
-                         {config(:lua), {:luerl, "~> 0.4.0"}},
+                         {config(:lua), {:luerl, "~> 0.3.1"}},
                          {config(:sqlite), {:sqlite3, "~> 1.1"}}], do:
       dep
   end
