6b0fa44386709151bb48ad9284a060888b7c5c54
Holger Weiss <holger@zedat.fu-berlin.de>
2021-10-09 10:56:30 +0200

    Update 'xmpp' dependency
    Fixes #3529.

--- mix.exs
+++ mix.exs
@@ -124,7 +124,7 @@ defmodule Ejabberd.MixProject do
      {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26"},
      {:stun, "~> 1.0"},
-     {:xmpp, git: "https://github.com/processone/xmpp", ref: "e943c0285aa85e3cbd4bfb9259f6b7de32b00395", override: true},
+     {:xmpp, git: "https://github.com/processone/xmpp", ref: "3f18c9e6b239c3a8a4d82dc478b26565037b7247", override: true},
      {:yconf, "~> 1.0"}]
     ++ cond_deps()
   end
