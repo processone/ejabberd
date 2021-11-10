132ebb8f2dbe222db0c4d025c131bb3e525939e8
Paweł Chmielowski <pawel@process-one.net>
2021-11-10 17:00:42 +0100

    Fix exception in mucsub {un}subscription events multicast handler
    While those event are wrapped in mucsub envelope they doesn't
    contain regular messages that require updating 'to' attribute,
    so don't process in that same way as events with wrapped
    message in them.

--- src/mod_muc_room.erl
+++ src/mod_muc_room.erl
@@ -5097,7 +5097,7 @@ send_subscriptions_change_notifications(From, Nick, Type, State) ->
 			id = p1_rand:get_string(),
 			sub_els = [Payload1]}]}}]},
 	ejabberd_router_multicast:route_multicast(State#state.jid, State#state.server_host,
-						  WJ, Packet1, true);
+						  WJ, Packet1, false);
 	true -> ok
     end,
     if WN /= [] ->
@@ -5113,7 +5113,7 @@ send_subscriptions_change_notifications(From, Nick, Type, State) ->
 			id = p1_rand:get_string(),
 			sub_els = [Payload2]}]}}]},
 	ejabberd_router_multicast:route_multicast(State#state.jid, State#state.server_host,
-					       WN, Packet2, true);
+					       WN, Packet2, false);
 	true -> ok
     end.
 
