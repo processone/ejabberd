217e930dc7b3b9cecc1ea2741e179d0a32ed0e00
Badlop <badlop@process-one.net>
2022-05-30 11:46:19 +0200

    Support @VERSION@ and @SEMVER@ in captcha_cmd option

--- src/ejabberd_options.erl
+++ src/ejabberd_options.erl
@@ -110,7 +110,14 @@ opt_type(cache_missed) ->
 opt_type(cache_size) ->
     econf:pos_int(infinity);
 opt_type(captcha_cmd) ->
-    econf:file();
+    econf:and_then(
+	econf:binary(),
+	fun(V) ->
+		V2 = misc:expand_keyword(<<"@SEMVER@">>, V,
+				    ejabberd_option:version()),
+		misc:expand_keyword(<<"@VERSION">>, V2,
+				    misc:semver_to_xxyy(ejabberd_option:version()))
+	end);
 opt_type(captcha_host) ->
     econf:binary();
 opt_type(captcha_limit) ->
