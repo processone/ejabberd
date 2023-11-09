7de0ba95d0a32d9857c1fcac5032a7f7fde4fd0a
Paweł Chmielowski <pawel@process-one.net>
2023-11-09 15:43:47 +0100

    Update mysql to bring sha256_password auth plugin support

--- mix.exs
+++ mix.exs
@@ -138,7 +138,7 @@ defmodule Ejabberd.MixProject do
                          {config(:zlib), {:ezlib, "~> 1.0"}},
                          {if_version_below('22', true), {:lager, "~> 3.9.1"}},
                          {config(:lua), {:luerl, "~> 1.0"}},
-                         {config(:mysql), {:p1_mysql, " >= 1.0.22"}},
+                         {config(:mysql), {:p1_mysql, git: "https://github.com/processone/p1_mysql.git", ref: "f685408b910c425b9905d4ddcdbedba717a5b48c"}},
                          {config(:pgsql), {:p1_pgsql, "~> 1.1"}},
                          {config(:sqlite), {:sqlite3, "~> 1.1"}},
                          {config(:stun), {:stun, "~> 1.0"}}], do:
