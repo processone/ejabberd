2438fc8b31047739cb0d0843cff0ed53bf92a2e6
Badlop <badlop@process-one.net>
2024-02-14 22:23:14 +0100

    Add OTP_BELOW_25 to mix.exs that was added to rebar.config in 4ae06f0

--- mix.exs
+++ mix.exs
@@ -81,7 +81,8 @@ defmodule Ejabberd.MixProject do
              if_version_below(~c"23", [{:d, :USE_OLD_CRYPTO_HMAC}]) ++
              if_version_below(~c"23", [{:d, :USE_OLD_PG2}]) ++
              if_version_below(~c"24", [{:d, :COMPILER_REPORTS_ONLY_LINES}]) ++
-             if_version_below(~c"24", [{:d, :SYSTOOLS_APP_DEF_WITHOUT_OPTIONAL}])
+             if_version_below(~c"24", [{:d, :SYSTOOLS_APP_DEF_WITHOUT_OPTIONAL}]) ++
+             if_version_below(~c"25", [{:d, :OTP_BELOW_25}])
     defines = for {:d, value} <- result, do: {:d, value}
     result ++ [{:d, :ALL_DEFS, defines}]
   end
