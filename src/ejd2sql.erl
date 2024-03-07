processone/ejabberd

commit cfc393a12e3507c9ecb541b8f784c14ff3a9786e

When exporting mod_mam, MUC entries are assigned to the MUC service (#3680)

src/ejd2sql.erl
@@ -73,11 +73,16 @@ export(Server, Output) ->
       end, Modules),
     close_output(Output, IO).
 
-export(Server, Output, Module1) ->
-    Module = case Module1 of
-		 mod_pubsub -> pubsub_db;
-		 _ -> Module1
-	     end,
+export(Server, Output, mod_mam = M1) ->
+    MucServices = gen_mod:get_module_opt_hosts(Server, mod_muc),
+    [export2(MucService, Output, M1, M1) || MucService <- MucServices],
+    export2(Server, Output, M1, M1);
+export(Server, Output, mod_pubsub = M1) ->
+    export2(Server, Output, M1, pubsub_db);
+export(Server, Output, M1) ->
+    export2(Server, Output, M1, M1).
+
+export2(Server, Output, Module1, Module) ->
     SQLMod = gen_mod:db_mod(sql, Module),
     LServer = jid:nameprep(iolist_to_binary(Server)),
     IO = prepare_output(Output),

commit 1639471250b4c8ead55c1699bab43efecd3bd8c3

Fix crash running export2sql when MAM enabled but MUC disabled
Problem introduced in cfc393a

src/ejd2sql.erl
@@ -74,7 +74,10 @@ export(Server, Output) ->
     close_output(Output, IO).
 
 export(Server, Output, mod_mam = M1) ->
-    MucServices = gen_mod:get_module_opt_hosts(Server, mod_muc),
+    MucServices = case gen_mod:is_loaded(Server, mod_muc) of
+        true -> gen_mod:get_module_opt_hosts(Server, mod_muc);
+        false -> []
+    end,
     [export2(MucService, Output, M1, M1) || MucService <- MucServices],
     export2(Server, Output, M1, M1);
 export(Server, Output, mod_pubsub = M1) ->