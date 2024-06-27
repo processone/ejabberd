28e94513db379ad50efc66f8df87177d00e718a8
Paweł Chmielowski <pawel@process-one.net>
2024-06-27 13:50:03 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -140,12 +140,12 @@ defmodule Ejabberd.MixProject do
      {:fast_yaml, "~> 1.0"},
      {:idna, "~> 6.0"},
      {:mqtree, "~> 1.0"},
-     {:p1_acme, git: "https://github.com/processone/p1_acme", branch: "master"},
+     {:p1_acme, "~> 1.0"},
      {:p1_oauth2, "~> 0.6"},
      {:p1_utils, "~> 1.0"},
-     {:pkix, git: "https://github.com/processone/pkix"},
+     {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26"},
-     {:xmpp, git: "https://github.com/processone/xmpp", override: true},
+     {:xmpp, ">= 1.8.2"},
      {:yconf, "~> 1.0"}]
     ++ cond_deps()
   end
@@ -175,8 +175,8 @@ defmodule Ejabberd.MixProject do
                          {if_version_below(~c"27", true), {:jiffy, "~> 1.1.1"}},
                          {if_version_below(~c"22", true), {:lager, "~> 3.9.1"}},
                          {config(:lua), {:luerl, "~> 1.2.0"}},
-                         {config(:mysql), {:p1_mysql, git: "https://github.com/processone/p1_mysql" }},
-                         {config(:pgsql), {:p1_pgsql, git: "https://github.com/processone/p1_pgsql"}},
+                         {config(:mysql), {:p1_mysql, ">= 1.0.24"}},
+                         {config(:pgsql), {:p1_pgsql, ">= 1.1.26"}},
                          {config(:sqlite), {:sqlite3, "~> 1.1"}},
                          {config(:stun), {:stun, "~> 1.0"}}], do:
       dep
