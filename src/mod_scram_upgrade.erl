c72ba1f18866a1d2f0f1dabca35d2c4e942ee54e
Badlop <badlop@process-one.net>
2024-12-03 12:45:53 +0100

    mod_scram_upgrade: Update XEP-0480 supported version (processone/xmpp#80)

--- src/mod_scram_upgrade.erl
+++ src/mod_scram_upgrade.erl
@@ -21,7 +21,7 @@
 %%%-------------------------------------------------------------------
 -module(mod_scram_upgrade).
 -behaviour(gen_mod).
--protocol({xep, 480, '0.1', '24.10', "complete", ""}).
+-protocol({xep, 480, '0.2.0', '24.10', "complete", ""}).
 
 %% gen_mod API
 -export([start/2, stop/1, reload/3, depends/2, mod_options/1, mod_opt_type/1]).
