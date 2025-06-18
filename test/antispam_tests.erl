f3b1b5d41935ee4c6e85d4f1dab5ed8ddca9d3b5
Badlop <badlop@process-one.net>
2025-06-18 11:35:06 +0200

    Result of running "make format"

--- test/antispam_tests.erl
+++ test/antispam_tests.erl
@@ -247,14 +247,13 @@ rtbl_domains_whitelisted(Config) ->
 spam_dump_file(Config) ->
     {ok, CWD} = file:get_cwd(),
     Filename = filename:join([CWD, "spam.log"]),
-    ?retry(100, 100,
-           ?match(true, size(get_bytes(Filename)) > 0)),
+    ?retry(100, 100, ?match(true, size(get_bytes(Filename)) > 0)),
     From = jid:make(<<"spammer_jid">>, <<"localhost">>, <<"spam_client">>),
     To = my_jid(Config),
     is_spam(message(From, To, <<"A very specific spam message">>)),
-    ?retry(100, 100,
-           ?match({match, _},
-                  re:run(get_bytes(Filename), <<"A very specific spam message">>))).
+    ?retry(100,
+           100,
+           ?match({match, _}, re:run(get_bytes(Filename), <<"A very specific spam message">>))).
 
 %%%===================================================================
 %%% Internal functions
