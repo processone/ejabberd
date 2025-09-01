378bf64fd5c6aa8e3afc1e3faf544eeed6940dac
Badlop <badlop@process-one.net>
2025-09-01 09:40:21 +0200

    Replace rebar3_format with efmt, improve emacs indentation script
    Revert partially "Add Makefile targets to format and indent source code"
    This reverts commit 3bda8582252c7a07fc848003e8facb0fad566431.

--- tools/emacs-indent.sh
+++ tools/emacs-indent.sh
@@ -1,18 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 # To indent and remove tabs, surround the piece of code with:
 #   %% @indent-begin
 #   %% @indent-end
 #
+# Install Emacs and erlang-mode. For example in Debian:
+#   apt-get install emacs erlang-mode
+#
 # Then run:
 #   make indent
 #
-# Please note this script only indents the first occurrence.
+# Please note this script only indents the first occurrence per file
 
-FILES=$(git grep --name-only @indent-begin src/)
+FILES=$(git grep --name-only @indent-begin include/ src/)
+LOG=${1:-/tmp/ejabberd-format.log}
+EMACS=${2:-emacs}
+
+if [ ! "$EMACS" ] || [ ! -x "$EMACS" ]
+then
+   echo "==> Cannot indent source code because Emacs is not installed"
+   exit 1
+fi
 
 for FILENAME in $FILES; do
-    echo "==> Indenting $FILENAME..."
+    echo "==> Indenting $FILENAME..." >>$LOG
     emacs -batch $FILENAME \
        -f "erlang-mode" \
         --eval "(goto-char (point-min))" \
@@ -23,5 +34,13 @@ for FILENAME in $FILES; do
         --eval "(erlang-indent-region begin end)" \
         --eval "(untabify begin end)" \
        -f "delete-trailing-whitespace" \
-       -f "save-buffer"
+       -f "save-buffer" >>$LOG 2>&1
 done
+
+grep -q 'Error' $LOG \
+    && cat $LOG
+grep -q 'Error: void-function (erlang-mode)' $LOG \
+    && echo \
+    && echo "==> Maybe you need to install erlang-mode system package" \
+    && exit 1
+rm $LOG
