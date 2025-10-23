c27a3a02fea2c1179c0f73bab82594b723a45be7
Pawel Chmielowski <pawel@process-one.net>
2025-10-23 13:38:02 +0200

    Tag dependences

--- mix.exs
+++ mix.exs
@@ -130,8 +130,8 @@ defmodule Ejabberd.MixProject do
      {:p1_utils, "~> 1.0"},
      {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26"},
-     {:xmpp, git: "https://github.com/processone/xmpp", ref: "e9d901ea84fd3910ad32b715853397eb1155b41c", override: true},
-     {:yconf, git: "https://github.com/processone/yconf", ref: "95692795a8a8d950ba560e5b07e6b80660557259", override: true}]
+     {:xmpp, ">= 1.11.2"},
+     {:yconf, ">= 1.0.22"}]
     ++ cond_deps()
   end
 
