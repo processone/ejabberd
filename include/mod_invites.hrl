a3a3f6f89e38f79019cde76702df00bedc05ce0e
Stefan Strigler <stefan@strigler.de>
2025-11-14 10:31:16 +0100

    set default for expires

--- include/mod_invites.hrl
+++ include/mod_invites.hrl
@@ -1,14 +1,15 @@
--record(invite_token, {token :: binary(),
-                       inviter :: {binary(), binary()},
-                       invitee = <<>> :: binary(),
-                       created_at = calendar:now_to_datetime(erlang:timestamp()) :: calendar:datetime(),
-                       expires :: calendar:datetime(),
-                       type = roster_only :: roster_only | account_only | account_subscription,
-                       account_name = <<>> :: binary()
-                      }).
-
 -define(INVITE_TOKEN_EXPIRE_SECONDS_DEFAULT, 5*86400).
 -define(INVITE_TOKEN_LENGTH_DEFAULT, 24).
 
 -define(NS_INVITE_INVITE, <<"urn:xmpp:invite#invite">>).
 -define(NS_INVITE_CREATE_ACCOUNT, <<"urn:xmpp:invite#create-account">>).
+
+-record(invite_token, {token :: binary(),
+                       inviter :: {binary(), binary()},
+                       invitee = <<>> :: binary(),
+                       created_at = calendar:now_to_datetime(erlang:timestamp()) :: calendar:datetime(),
+                       expires = calendar:gregorian_seconds_to_datetime(calendar:datetime_to_gregorian_seconds(calendar:now_to_datetime(erlang:timestamp()))
+                                                                        + ?INVITE_TOKEN_EXPIRE_SECONDS_DEFAULT) :: calendar:datetime(),
+                       type = roster_only :: roster_only | account_only | account_subscription,
+                       account_name = <<>> :: binary()
+                      }).
