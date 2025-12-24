processone/ejabberd

commit 06a8994f246b5928c366c4fdfd15f52afa55f26a

ejabberd_http: Add helper get_auto_urls/2 that returns all URLs and TLS

src/ejabberd_http.erl
@@ -33,8 +33,10 @@
 	 accept/1, receive_headers/1, recv_file/2,
 	 listen_opt_type/1, listen_options/0,
 	 apply_custom_headers/2]).
--export([get_url/4, get_auto_url/2, find_handler_port_path/2]).
+-export([get_url/4, get_auto_url/2, get_auto_urls/2, find_handler_port_path/2]).
 -export([init/3]).
+
+-deprecate({get_auto_url, 2}).
 
 -include("logger.hrl").
 -include_lib("xmpp/include/xmpp.hrl").
@@ -896,21 +898,29 @@ get_url_preliminar(M, Tls, Host, Option, Handler) ->
     end.
 
 get_auto_url(Tls, Handler) ->
-    case find_handler_port_path(Tls, Handler) of
-        [] -> undefined;
-        [{ThisTls, Port, Path} | _] ->
+    [{_ThisTls, Url} | _] = get_auto_urls(Tls, Handler),
+    Url.
+
+-spec get_auto_urls(boolean() | any, atom()) -> [{boolean(), binary()}].
+
+get_auto_urls(Tls, Handler) ->
+    Paths = find_handler_port_path(Tls, Handler),
+    [prepare_url(Path, Handler) || Path <- Paths].
+
+prepare_url({ThisTls, Port, Path}, Handler) ->
             Protocol = case {ThisTls, Handler} of
                            {false, ejabberd_http_ws} -> <<"ws">>;
                            {true, ejabberd_http_ws} -> <<"wss">>;
                            {false, _} -> <<"http">>;
                            {true, _} -> <<"https">>
                        end,
-            <<Protocol/binary,
+            {ThisTls,
+             <<Protocol/binary,
               "://@HOST@:",
               (integer_to_binary(Port))/binary,
               "/",
-              (str:join(Path, <<"/">>))/binary>>
-    end.
+              (str:join(Path, <<"/">>))/binary,
+              "/">>}.
 
 find_handler_port_path(Tls, Handler) ->
     lists:filtermap(

commit 8fddc9665f8e0ed3b16262b8d16ab6d511fb5612

ejabberd_http: Get back handling when BOSH or WS are disabled
This support was broken in recent commit 06a8994f2

src/ejabberd_http.erl
@@ -898,8 +898,10 @@ get_url_preliminar(M, Tls, Host, Option, Handler) ->
     end.
 
 get_auto_url(Tls, Handler) ->
-    [{_ThisTls, Url} | _] = get_auto_urls(Tls, Handler),
-    Url.
+    case get_auto_urls(Tls, Handler) of
+        [] -> undefined;
+        [{_ThisTls, Url} | _] -> Url
+    end.
 
 -spec get_auto_urls(boolean() | any, atom()) -> [{boolean(), binary()}].
 