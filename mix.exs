b71fcfc82fbbc4d08be1689757fd5aa55265e8e1
Pawel Chmielowski <pawel@process-one.net>
2026-01-19 12:43:01 +0100

    Use tagged dependencies

--- mix.exs
+++ mix.exs
@@ -119,7 +119,7 @@ defmodule Ejabberd.MixProject do
      {:p1_utils, "~> 1.0"},
      {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26"},
-     {:xmpp, git: "https://github.com/processone/xmpp", ref: "7285aa7802bfa90bcefafdad3a342fbb93ce7eea", override: true},
+     {:xmpp, ">= 1.12.0"},
      {:yconf, ">= 1.0.22"}]
     ++ cond_deps()
   end
@@ -148,8 +148,8 @@ defmodule Ejabberd.MixProject do
                          {if_version_below(~c"26", true), {:jose, "1.11.10", override: true}},
                          {if_version_above(~c"25", true), {:jose, "~> 1.11.12"}},
                          {config(:lua), {:luerl, "~> 1.2.0"}},
-                         {config(:mysql), {:p1_mysql, git: "https://github.com/processone/p1_mysql", ref: "ddd19938519c5f0319e0e0fe18ae98f856d64041"}},
-                         {config(:pgsql), {:p1_pgsql, ">= 1.1.32"}},
+                         {config(:mysql), {:p1_mysql, ">= 1.0.27"}},
+                         {config(:pgsql), {:p1_pgsql, ">= 1.1.38"}},
                          {config(:sqlite), {:sqlite3, "~> 1.1"}},
                          {config(:stun), {:stun, "~> 1.0"}}], do:
       dep
