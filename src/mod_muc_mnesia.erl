processone/ejabberd

commit 2b7285e0b2423fb54f54cd41342d02658e178c57

Update implementation of XEP-0317 Hats to version 0.3.1 (#4380)

src/mod_muc_mnesia.erl
@@ -429,11 +429,15 @@ need_transform({muc_room, {N, H}, _})
     ?INFO_MSG("Mnesia table 'muc_room' will be converted to binary", []),
     true;
 need_transform({muc_room, {_N, _H}, Opts}) ->
-    case lists:keymember(allow_private_messages, 1, Opts) of
-        true ->
+    case {lists:keymember(allow_private_messages, 1, Opts),
+          lists:keymember(hats_defs, 1, Opts)} of
+        {true, _} ->
             ?INFO_MSG("Mnesia table 'muc_room' will be converted to allowpm", []),
             true;
-        false ->
+        {false, false} ->
+            ?INFO_MSG("Mnesia table 'muc_room' will be converted to Hats 0.3.0", []),
+            true;
+        {false, true} ->
             false
     end;
 
@@ -459,7 +463,33 @@ transform(#muc_room{opts = Opts} = R) ->
         _ ->
             Opts
     end,
-    R#muc_room{opts = Opts2};
+    Opts4 =
+        case lists:keyfind(hats_defs, 1, Opts2) of
+            false ->
+                {hats_users, HatsUsers} = lists:keyfind(hats_users, 1, Opts2),
+                {HatsDefs, HatsUsers2} =
+                    lists:foldl(fun({Jid, UriTitleList}, {Defs, Assigns}) ->
+                                   Defs2 =
+                                       lists:foldl(fun({Uri, Title}, AccDef) ->
+                                                      maps:put(Uri, {Title, <<"">>}, AccDef)
+                                                   end,
+                                                   Defs,
+                                                   UriTitleList),
+                                   Assigns2 =
+                                       maps:put(Jid,
+                                                [Uri || {Uri, _Title} <- UriTitleList],
+                                                Assigns),
+                                   {Defs2, Assigns2}
+                                end,
+                                {maps:new(), maps:new()},
+                                HatsUsers),
+                Opts3 =
+                    lists:keyreplace(hats_users, 1, Opts2, {hats_users, maps:to_list(HatsUsers2)}),
+                [{hats_defs, maps:to_list(HatsDefs)} | Opts3];
+            {_, _} ->
+                Opts2
+        end,
+    R#muc_room{opts = Opts4};
 transform(#muc_registered{us_host = {{U, S}, H}, nick = Nick} = R) ->
     R#muc_registered{us_host = {{iolist_to_binary(U), iolist_to_binary(S)},
 				iolist_to_binary(H)},

ejabberd.doap
@@ -578,10 +578,10 @@
     <implements>
       <xmpp:SupportedXep>
         <xmpp:xep rdf:resource="https://xmpp.org/extensions/xep-0317.html"/>
-        <xmpp:version>0.2.0</xmpp:version>
+        <xmpp:version>0.3.1</xmpp:version>
         <xmpp:since>21.12</xmpp:since>
         <xmpp:status>complete</xmpp:status>
-        <xmpp:note>mod_muc_room, 0.2.0 since 25.03</xmpp:note>
+        <xmpp:note>mod_muc_room, 0.3.1 since 25.xx</xmpp:note>
       </xmpp:SupportedXep>
     </implements>
     <implements>

include/mod_muc_room.hrl
@@ -126,7 +126,8 @@
     history                 = #lqueue{} :: lqueue(),
     subject                 = [] :: [text()],
     subject_author          = {<<"">>, #jid{}} :: {binary(), jid()},
-    hats_users              = #{} :: #{ljid() => #{binary() => binary()}},
+    hats_defs               = #{} :: #{binary() => {binary(), binary()}},
+    hats_users              = #{} :: #{ljid() => [binary()]},
     just_created            = erlang:system_time(microsecond) :: true | integer(),
     activity                = treap:empty() :: treap:treap(),
     room_shaper             = none :: ejabberd_shaper:shaper(),

mix.exs
@@ -129,7 +129,7 @@ defmodule Ejabberd.MixProject do
      {:p1_utils, "~> 1.0"},
      {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26"},
-     {:xmpp, ">= 1.11.1"},
+     {:xmpp, git: "https://github.com/processone/xmpp", ref: "e9d901ea84fd3910ad32b715853397eb1155b41c", override: true},
      {:yconf, git: "https://github.com/processone/yconf", ref: "95692795a8a8d950ba560e5b07e6b80660557259", override: true}]
     ++ cond_deps()
   end

rebar.config
@@ -77,7 +77,7 @@
         {stringprep, "~> 1.0.33", {git, "https://github.com/processone/stringprep", {tag, "1.0.33"}}},
         {if_var_true, stun,
          {stun, "~> 1.2.21", {git, "https://github.com/processone/stun", {tag, "1.2.21"}}}},
-        {xmpp, "~> 1.11.1", {git, "https://github.com/processone/xmpp", {tag, "1.11.1"}}},
+        {xmpp, ".*", {git, "https://github.com/processone/xmpp", "e9d901ea84fd3910ad32b715853397eb1155b41c"}},
         {yconf, ".*", {git, "https://github.com/processone/yconf", "95692795a8a8d950ba560e5b07e6b80660557259"}}
        ]}.
 

src/mod_muc.erl
@@ -1766,7 +1766,7 @@ mod_doc() ->
                        "The default value is an empty string.")}},
              {enable_hats,
               #{value => "true | false",
-                note => "improved in 25.03",
+                note => "improved in 25.xx",
                 desc =>
                     ?T("Allow extended roles as defined in XEP-0317 Hats. "
                        "Check the _`../../tutorials/muc-hats.md|MUC Hats`_ tutorial. "

commit 7c2db2aebb6731520fdc76694a1c798065d69efa

mod_muc_mnesia: Fix bug introduced in 2b7285e when upgrading old database
If mod_muc uses Mnesia database,
and there is any room created using ejabberd older than 21.12,
and its room config was never modified using newer ejabberd versions...
in that case the room configuration does not contain hats_users.

When upgrading to ejabberd 25.10 or 26.01,
it tries to upgrade the MUC rooms configuration but fails with error:
  ejabberd_mnesia:transform_fun/2:382
  Failed to transform Mnesia table muc_room

src/mod_muc_mnesia.erl
@@ -478,9 +478,11 @@ transform(#muc_room{opts = Opts} = R) ->
             Opts
     end,
     Opts4 =
-        case lists:keyfind(hats_defs, 1, Opts2) of
-            false ->
-                {hats_users, HatsUsers} = lists:keyfind(hats_users, 1, Opts2),
+        case {lists:keyfind(hats_defs, 1, Opts2),
+              lists:keyfind(hats_users, 1, Opts2)} of
+            {false, false} ->
+                [{hats_defs, []}, {hats_users, []} | Opts2];
+            {false, {hats_users, HatsUsers}} ->
                 {HatsDefs, HatsUsers2} =
                     lists:foldl(fun({Jid, UriTitleList}, {Defs, Assigns}) ->
                                    Defs2 =
@@ -500,7 +502,7 @@ transform(#muc_room{opts = Opts} = R) ->
                 Opts3 =
                     lists:keyreplace(hats_users, 1, Opts2, {hats_users, maps:to_list(HatsUsers2)}),
                 [{hats_defs, maps:to_list(HatsDefs)} | Opts3];
-            {_, _} ->
+            {{hats_defs, _}, {hats_users, _}} ->
                 Opts2
         end,
     R#muc_room{opts = Opts4};