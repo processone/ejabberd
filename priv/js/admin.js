a81690681fcdb809f07e70d916fcff9bbf88c357
Badlop <badlop@process-one.net>
2026-06-09 14:06:32 +0200

    Remove JS functions that are not used sice commit 5a34020

--- priv/js/admin.js
+++ priv/js/admin.js
@@ -1,19 +1,3 @@
-
-function selectAll() {
-  for(i=0;i<document.forms[0].elements.length;i++)
-  { var e = document.forms[0].elements[i];
-    if(e.type == 'checkbox')
-    { e.checked = true; }
-  }
-}
-function unSelectAll() {
-  for(i=0;i<document.forms[0].elements.length;i++)
-  { var e = document.forms[0].elements[i];
-    if(e.type == 'checkbox')
-    { e.checked = false; }
-  }
-}
-
 (function () {
     'use strict';
     window.addEventListener('load', () => {
