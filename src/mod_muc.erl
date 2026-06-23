09828468042e37837c81bd25944070368d5cda5f
Badlop <badlop@process-one.net>
2026-06-23 13:42:01 +0200

    mod_muc: Bump XEP-0045 to 1.35.5

--- src/mod_muc.erl
+++ src/mod_muc.erl
@@ -24,7 +24,7 @@
 %%%----------------------------------------------------------------------
 -module(mod_muc).
 -author('alexey@process-one.net').
--protocol({xep, 45, '1.35.3', '0.5.0', "complete", ""}).
+-protocol({xep, 45, '1.35.5', '0.5.0', "complete", ""}).
 -protocol({xep, 249, '1.2', '0.5.0', "complete", ""}).
 -protocol({xep, 421, '1.0.1', '23.10', "complete", ""}).
 -protocol({xep, 486, '0.1.0', '24.07', "complete", ""}).
